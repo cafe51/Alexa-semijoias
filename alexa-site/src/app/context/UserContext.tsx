a75f0b0f57bf6c48ee5bebab7337173b98a25402
cafe51 <cafecafe51@hotmail.com>
2024-05-23 18:39:27 -0300

    add context file

---
+++ alexa-site/src/app/context/UserContext.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { UserType } from '../utils/types';
+
+
+interface UserContextType {
+  user: UserType | null;
+  setUser: (user: UserType | null) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<UserType | null>(null);
+
+    return (
+        <UserContext.Provider value={ { user, setUser } }>
+            { children }
+        </UserContext.Provider>
+    );
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+    return context;
+};
