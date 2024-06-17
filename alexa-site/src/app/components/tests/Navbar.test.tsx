5de846e0cd788017a326f5dca3877151401def71
cafe51 <cafecafe51@hotmail.com>
2024-06-17 20:54:18 -0300

    created test for NavBar.ts

---
+++ alexa-site/src/app/components/tests/Navbar.test.tsx
@@ -0,0 +1,60 @@
+// Navbar.test.tsx
+
+import { act, render, screen } from '@testing-library/react';
+import Navbar from '../Navbar';
+import { AuthContextProvider } from '@/app/context/AuthContext';
+import { UserInfoProvider } from '@/app/context/UserInfoContext';
+
+jest.mock('next/navigation', () => ({
+    useRouter: () => ({
+        push: jest.fn(),
+    }),
+}));
+
+jest.mock('../../hooks/useCollection', () => ({
+    useCollection: jest.fn(),
+}));
+
+jest.mock('../../hooks/useAuthContext', () => ({
+    useAuthContext: jest.fn(),
+}));
+
+jest.mock('../../hooks/useUserInfo', () => ({
+    useUserInfo: jest.fn(),
+}));
+
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useUserInfo } from '../../hooks/useUserInfo';
+import { useCollection } from '../../hooks/useCollection';
+
+
+describe('Navbar Component', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        (useCollection as jest.Mock).mockReturnValue({ documents: [] });
+        (useAuthContext as jest.Mock).mockReturnValue({ user: null, authIsReady: false });
+        (useUserInfo as jest.Mock).mockReturnValue({ userInfo: null, carrinho: [] });
+
+    });
+
+    it('Renderiza os links de navegação', async() => {
+        await act(async() => {
+            render(
+                <AuthContextProvider>
+                    <UserInfoProvider>
+                        <Navbar isMenuOpen={ false } setIsMenuOpen={ jest.fn() } /> 
+                    </UserInfoProvider>
+                </AuthContextProvider>,
+            );
+        });
+        expect(screen.getByText('Início')).toBeInTheDocument();
+        expect(screen.getByText('Brincos')).toBeInTheDocument();
+        expect(screen.getByText('Pulseiras')).toBeInTheDocument();
+        expect(screen.getByText('Colares')).toBeInTheDocument();
+        expect(screen.getByText('Anéis')).toBeInTheDocument();
+        expect(screen.getByText('Fale Comigo')).toBeInTheDocument();
+    });
+
+});
+
+// falta testar a navegação
