13f7611c1daaa787e102a4b2b8432ec9dc730b04
cafe51 <cafecafe51@hotmail.com>
2024-07-05 11:52:50 -0300

    fixed useCart.test.tsx

--- alexa-site/src/app/hooks/tests/useCart.test.tsx
+++ alexa-site/src/app/hooks/tests/useCart.test.tsx
@@ -4,8 +4,13 @@ import { renderHook, waitFor } from '@testing-library/react';
 import { useCart } from '../useCart';
 import { useCollection } from '../useCollection';
 import { CartInfoType, ProductType } from '../../utils/types';
+import { useAuthContext } from '../useAuthContext';
 
 jest.mock('../useCollection');
+
+jest.mock('../../hooks/useAuthContext', () => ({
+    useAuthContext: jest.fn(),
+}));
 
 describe('useCart Hook', () => {
     
@@ -36,16 +41,22 @@ describe('useCart Hook', () => {
         },
     ];
 
+    const mockUser = {
+        uid: 'user1',
+    };
+
     const mockCartItems: CartInfoType[] = [
         { productId: 'prod1', quantidade: 2, userId: 'user1' },
         { productId: 'prod3', quantidade: 1, userId: 'user1' }, // Produto 3 não existe na lista de produtos
     ];
 
     it('mapeia corretamente os produtos do carrinho com seus detalhes', async() => {
+        (useAuthContext as jest.Mock).mockReturnValue({ user: mockUser, authIsReady: true });
+
         (useCollection as jest.Mock).mockReturnValue({
         });
 
-        const { result } = renderHook(() => useCart(mockCartItems, mockProducts));
+        const { result } = renderHook(() => useCart(mockCartItems, mockProducts, () => {}));
 
         await waitFor(() => expect(result.current.mappedProducts).toEqual([
             {
