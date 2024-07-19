e4696e9f97be533850089226ea3f1bfdbb36d309
cafe51 <cafecafe51@hotmail.com>
2024-07-19 20:48:33 -0300

    RegisterSection on checkout page split in a new component RegisterForm

--- alexa-site/src/app/checkout/AccountSection/RegisterSection.tsx
+++ alexa-site/src/app/checkout/AccountSection/RegisterSection.tsx
@@ -1,25 +1,17 @@
 // app/checkout/AccountSection/AccountSection.tsx
-
 import { useState } from 'react';
-import InputField from '../AddressSection/InputField';
+import RegisterForm from '@/app/components/RegisterForm';
 
 interface RegisterSectionProps {
   setShowRegister: (isLogin: boolean) => void;
 }
 
 export default function RegisterSection({ setShowRegister }: RegisterSectionProps) {
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [loadingButton, setLoadingButton] = useState(true);
 
     return (
-        <section className="border p-4 rounded-md shadow-md bg-white">
-
-
+        <section className="flex flex-col border p-4 rounded-md shadow-md bg-white gap-4">
             <div className='flex justify-between w-full'>
-
                 <p className="font-bold">Cadastro</p>
                 <p
                     className='text-blue-400 text-sm w-full text-end'
@@ -29,47 +21,7 @@ export default function RegisterSection({ setShowRegister }: RegisterSectionProp
                 </p>
             </div>
 
-
-            <div className='flex flex-col w-full p-2'>
-                <InputField
-                    id="nome"
-                    value={ nome }
-                    onChange={ (e) => setNome(e.target.value) }
-                    label="Nome"
-                />
-                <InputField
-                    id="email"
-                    value={ email }
-                    onChange={ (e) => setEmail(e.target.value) }
-                    label="Email"
-                />
-                <InputField
-                    id="telefone"
-                    value={ telefone }
-                    onChange={ (e) => setTelefone(e.target.value) }
-                    label="Telefone"
-                />
-                <InputField
-                    id="password"
-                    type='password'
-                    value={ password }
-                    onChange={ (e) => setPassword(e.target.value) }
-                    label="Password"
-                />
-                <InputField
-                    id="confirmPassword"
-                    type='password'
-                    value={ confirmPassword }
-                    onChange={ (e) => setConfirmPassword(e.target.value) }
-                    label="ConfirmPassword"
-                />
-
-                <button className="w-full bg-green-500 text-white p-2 rounded">
-              Continuar
-                </button>
-            </div>
-            
-
+            <RegisterForm loadingButton={ loadingButton } setLoadingButton={ setLoadingButton } />
         </section>
     );
 }
