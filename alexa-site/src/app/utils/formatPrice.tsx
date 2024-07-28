c0e94534f1b6eb4a3caa6f9393706352322483cb
cafe51 <cafecafe51@hotmail.com>
2024-07-28 18:00:15 -0300

    formatPrice now accept invalid parameter returning 0

--- alexa-site/src/app/utils/formatPrice.tsx
+++ alexa-site/src/app/utils/formatPrice.tsx
@@ -1,11 +1,13 @@
 //app/utils/formatPrice.tsx
 
-export default function formatPrice(valor: number): string {
+export default function formatPrice(valor: number | null | undefined): string {
     const formatador = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
         minimumFractionDigits: 2, // Garante sempre duas casas decimais
     });
+
+    if (!valor) return formatador.format(0);
 
     return formatador.format(valor);
 }
