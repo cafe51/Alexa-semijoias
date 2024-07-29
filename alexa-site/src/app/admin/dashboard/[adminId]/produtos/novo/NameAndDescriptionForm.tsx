295bd495869ed137f793c509d74a03d1f296bd40
cafe51 <cafecafe51@hotmail.com>
2024-07-29 15:34:33 -0300

    create newProducts screen page and NameAndDescriptionSection as the first section

---
+++ alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/NameAndDescriptionForm.tsx
@@ -0,0 +1,39 @@
+import { UseNewProductStateType } from '@/app/utils/types';
+
+interface NameAndDescriptionSectionProps {
+    state: UseNewProductStateType;
+    handleNameChange: (name: string) => void;
+    handleDescriptionChange: (description: string) => void; 
+}
+
+export default function NameAndDescriptionSection({ state: { name, description }, handleNameChange, handleDescriptionChange }: NameAndDescriptionSectionProps) {
+
+    return (
+        <section className="p-4 border rounded-md bg-white">
+            <h2 className="text-lg font-bold">Nome e descrição</h2>
+            <div className="mt-2">
+                <label className="block text-sm font-medium" htmlFor='name'>Nome</label>
+                <input
+                    id='name'
+                    name='name'
+                    type="text"
+                    value={ name }
+                    onChange={ (e) => handleNameChange(e.target.value) }
+                    className="mt-1 block w-full px-3 py-2 border rounded-md"
+                    placeholder="Nome do produto"
+                />
+            </div>
+            <div className="mt-2">
+                <label className="block text-sm font-medium" htmlFor='description'>Descrição</label>
+                <textarea
+                    id='description'
+                    name='description'
+                    value={ description }
+                    onChange={ (e) => handleDescriptionChange(e.target.value) }
+                    className="mt-1 block w-full px-3 py-2 border rounded-md min-h-44"
+                    placeholder="Descrição do produto"
+                />
+            </div>
+        </section>
+    );
+}
---
+++ alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useNewProductState } from '@/app/hooks/useNewProductState';
+import NameAndDescriptionSection from './NameAndDescriptionForm';
+import LargeButton from '@/app/components/LargeButton';
+
+export default function NewProductPage() {
+    const {
+        state,
+        handleNameChange,
+        handleDescriptionChange,
+    } = useNewProductState();
+    return (
+        <main className='flex flex-col gap-2'>
+            <NameAndDescriptionSection
+                state={ state }
+                handleNameChange={ handleNameChange }
+                handleDescriptionChange={ handleDescriptionChange }
+            />
+            <LargeButton color='blue' onClick={ () => console.log(state) } loadingButton={ false }>
+            mostrar estado
+            </LargeButton>
+        </main>
+    );
+}
