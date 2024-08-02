74b084bb905f1c7a17fbc8b16755b75ad5b3dfb9
cafe51 <cafecafe51@hotmail.com>
2024-08-02 14:27:20 -0300

    fix css

--- alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/AssocietedProductsSection.tsx
+++ alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/AssocietedProductsSection.tsx
@@ -4,7 +4,7 @@ export default function AssocietedProductsSection() {
     return (
         <section className="p-4 border rounded-md bg-white">
             <h2 className="text-lg font-bold">Produtos Associados</h2>
-            <div className="mt-2">
+            <div className="mt-2 border-t border-solid text-center w-full">
                 <button className="text-blue-500">Adicionar produtos associados</button>
             </div>
         </section>
--- alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/CategoriesSection.tsx
+++ alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/CategoriesSection.tsx
@@ -4,7 +4,7 @@ export default function CategoriesSection() {
     return (
         <section className="p-4 border rounded-md bg-white">
             <h2 className="text-lg font-bold">Categorias</h2>
-            <div className="mt-2">
+            <div className="mt-2 border-t border-solid text-center w-full">
                 <button className="text-blue-500">Adicionar categorias</button>
             </div>
         </section>
