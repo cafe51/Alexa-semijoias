446fff936622291bc22d49ea9ebf5d922d867a95
cafe51 <cafecafe51@hotmail.com>
2024-08-07 12:48:58 -0300

    formated display of dimensions section on new product page

--- alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/DimensionsSection.tsx
+++ alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/DimensionsSection.tsx
@@ -13,55 +13,57 @@ export default function DimensionsSection({
     handleDimensionsChange,
 }: DimensionsSectionProps) {
     return (
-        <section className="p-4 border rounded-md bg-white">
-            <h2 className="text-lg font-bold">Peso e dimensões</h2>
-            <div className="mt-2">
-                <label htmlFor='weight' className="block text-sm font-medium">Peso</label>
-                <input
-                    id='weight'
-                    name='weight'                                
-                    type="number"
-                    value={ state.dimensions.weight }
-                    onChange={ (e) => handleDimensionsChange({ ...state.dimensions, weight: Number(e.target.value) }) }
-                    className="mt-1 block w-full px-3 py-2 border rounded-md"
-                    placeholder="Peso"
-                />
-            </div>
-            <div className="mt-2">
-                <label htmlFor='length' className="block text-sm font-medium">Comprimento</label>
-                <input
-                    id='length'
-                    name='length'
-                    type="number"
-                    value={ state.dimensions.length }
-                    onChange={ (e) => handleDimensionsChange({ ...state.dimensions, length: Number(e.target.value) }) }
-                    className="mt-1 block w-full px-3 py-2 border rounded-md"
-                    placeholder="Comprimento"
-                />
-            </div>
-            <div className="mt-2">
-                <label htmlFor='width' className="block text-sm font-medium">Largura</label>
-                <input
-                    id='width'
-                    name='width'
-                    type="number"
-                    value={ state.dimensions.width }
-                    onChange={ (e) => handleDimensionsChange({ ...state.dimensions, width: Number(e.target.value) }) }
-                    className="mt-1 block w-full px-3 py-2 border rounded-md"
-                    placeholder="Largura"
-                />
-            </div>
-            <div className="mt-2">
-                <label htmlFor='height' className="block text-sm font-medium">Altura</label>
-                <input
-                    id='height'
-                    name='height'
-                    type="number"
-                    value={ state.dimensions.height }
-                    onChange={ (e) => handleDimensionsChange({ ...state.dimensions, height: Number(e.target.value) }) }
-                    className="mt-1 block w-full px-3 py-2 border rounded-md"
-                    placeholder="Altura"
-                />
+        <section className="flex flex-col gap-2 p-2 py-4 border rounded-md bg-white w-full">
+            <h2 className="font-bold  p-2">Peso e dimensões</h2>
+            <div className='flex flex-wrap justify-center w-full gap-4 text-xs'>
+                <div className="w-5/12">
+                    <label htmlFor='weight' className="text-xs font-medium w-full">Peso</label>
+                    <input
+                        id='weight'
+                        name='weight'                                
+                        type="number"
+                        value={ state.dimensions.weight }
+                        onChange={ (e) => handleDimensionsChange({ ...state.dimensions, weight: Number(e.target.value) }) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md"
+                        placeholder="Peso"
+                    />
+                </div>
+                <div className="w-5/12">
+                    <label htmlFor='length' className="text-xs font-medium w-full">Comprimento</label>
+                    <input
+                        id='length'
+                        name='length'
+                        type="number"
+                        value={ state.dimensions.length }
+                        onChange={ (e) => handleDimensionsChange({ ...state.dimensions, length: Number(e.target.value) }) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md"
+                        placeholder="Comprimento"
+                    />
+                </div>
+                <div className="w-5/12">
+                    <label htmlFor='width' className="text-xs font-medium w-full">Largura</label>
+                    <input
+                        id='width'
+                        name='width'
+                        type="number"
+                        value={ state.dimensions.width }
+                        onChange={ (e) => handleDimensionsChange({ ...state.dimensions, width: Number(e.target.value) }) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md"
+                        placeholder="Largura"
+                    />
+                </div>
+                <div className="w-5/12">
+                    <label htmlFor='height' className="text-xs font-medium w-full">Altura</label>
+                    <input
+                        id='height'
+                        name='height'
+                        type="number"
+                        value={ state.dimensions.height }
+                        onChange={ (e) => handleDimensionsChange({ ...state.dimensions, height: Number(e.target.value) }) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md"
+                        placeholder="Altura"
+                    />
+                </div>
             </div>
         </section>
     );
