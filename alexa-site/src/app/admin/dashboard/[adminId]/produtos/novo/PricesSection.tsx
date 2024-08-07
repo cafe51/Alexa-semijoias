85b49350e9985d089ab41de85d6f6495c28213ee
cafe51 <cafecafe51@hotmail.com>
2024-08-07 12:31:11 -0300

    formated price section display on new product screen page

--- alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/PricesSection.tsx
+++ alexa-site/src/app/admin/dashboard/[adminId]/produtos/novo/PricesSection.tsx
@@ -7,49 +7,73 @@ interface PricesSectionProps {
     handleValueChange: (value: { price: number, promotionalPrice: number, cost: number, }) => void;
 }
 
+
+
 export default function PricesSection({
-    state,
+    state: { value },
     handleValueChange,
 } : PricesSectionProps){
+
+    const marginProfitValue = (v: typeof value) => {
+        const finalPrice = v.promotionalPrice && v.promotionalPrice > 0 ? v.promotionalPrice : v.price;
+        const profit = (finalPrice - v.cost);
+        const margin = profit/finalPrice;
+        return margin * 100 ;
+    };
+
     return (
-        <section className="p-4 border rounded-md bg-white">
-            <h2 className="text-lg font-bold">Preços</h2>
-            <div className="mt-2">
-                <label htmlFor='price' className="block text-sm font-medium">Preço de venda</label>
-                <input
-                    id='price'
-                    name='price'
-                    type="number"
-                    value={ state.value.price }
-                    onChange={ (e) => handleValueChange({ ...state.value, price: Number(e.target.value) }) }
-                    className="mt-1 block w-full px-3 py-2 border rounded-md"
-                    placeholder="R$ 0,00"
-                />
+        <section className="flex flex-col gap-2 p-2 py-4 border rounded-md bg-white w-full">
+            <h2 className="font-bold p-2">Preços</h2>
+            <div className='flex flex-wrap justify-center w-full gap-4 text-xs'>
+                <div className="w-5/12">
+                    <label htmlFor='price' className="text-xs font-medium w-full">Preço de venda</label>
+                    <input
+                        id='price'
+                        name='price'
+                        type="number"
+                        value={ value.price }
+                        onChange={ (e) => handleValueChange({ ...value, price: Number(e.target.value) }) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md"
+                        placeholder="R$ 0,00"
+                    />
+                </div>
+                <div className="w-5/12">
+                    <label htmlFor='promotionalPrice' className="text-xs font-medium">Preço promocional</label>
+                    <input
+                        id='promotionalPrice'
+                        name='promotionalPrice'
+                        type="number"
+                        value={ value.promotionalPrice }
+                        onChange={ (e) => handleValueChange({ ...value, promotionalPrice: Number(e.target.value) }) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md"
+                        placeholder="R$ 0,00"
+                    />
+                </div>
+                <div className="w-5/12">
+                    <label htmlFor='cost' className="text-xs font-medium">Custo</label>
+                    <input
+                        id='cost'
+                        name='cost'
+                        type="number"
+                        value={ value.cost }
+                        onChange={ (e) => handleValueChange({ ...value, cost: Number(e.target.value) }) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md"
+                        placeholder="R$ 0,00"
+                    />
+                </div>
+                <div className="w-5/12">
+                    <label htmlFor='cost' className="text-xs font-medium">Margem de Lucro</label>
+                    <input
+                        id='profitMargin'
+                        name='profitMargin'
+                        type="number"
+                        value={ marginProfitValue(value) }
+                        className="mt-1 w-full px-3 py-2 border rounded-md bg-gray-100"
+                        readOnly
+                    />
+                </div>
             </div>
-            <div className="mt-2">
-                <label htmlFor='promotionalPrice' className="block text-sm font-medium">Preço promocional</label>
-                <input
-                    id='promotionalPrice'
-                    name='promotionalPrice'
-                    type="number"
-                    value={ state.value.promotionalPrice }
-                    onChange={ (e) => handleValueChange({ ...state.value, promotionalPrice: Number(e.target.value) }) }
-                    className="mt-1 block w-full px-3 py-2 border rounded-md"
-                    placeholder="R$ 0,00"
-                />
-            </div>
-            <div className="mt-2">
-                <label htmlFor='cost' className="block text-sm font-medium">Custo</label>
-                <input
-                    id='cost'
-                    name='cost'
-                    type="number"
-                    value={ state.value.cost }
-                    onChange={ (e) => handleValueChange({ ...state.value, cost: Number(e.target.value) }) }
-                    className="mt-1 block w-full px-3 py-2 border rounded-md"
-                    placeholder="R$ 0,00"
-                />
-            </div>
+
         </section>
     );
 }
