0d86cb86b3b4d320f0709c0dbe0d463b0d720ecb
cafe51 <cafecafe51@hotmail.com>
2024-09-29 03:33:37 -0300

    fix: type error

--- alexa-site/src/app/checkout/PaymentSection/PaymentSection.tsx
+++ alexa-site/src/app/checkout/PaymentSection/PaymentSection.tsx
@@ -64,7 +64,7 @@ export default function PaymentSection({ cartPrice, state, handleSelectedPayment
                 //     { skuId, name, barcode, categories, productId, quantidade, image, value, customProperties }
                 // )),
                 cartSnapShot: carrinho,
-                status: 'pendente',
+                status: 'aguardando pagamento',
                 userId: userInfo.id,
                 valor: {
                     frete: deliveryPrice,
