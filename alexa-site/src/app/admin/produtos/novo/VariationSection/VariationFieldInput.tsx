069efb76abe576e6fb0a94da225f4a4a7aa6f9f0
cafe51 <cafecafe51@hotmail.com>
2024-10-01 10:49:24 -0300

    fix: VariationFieldInput is now showing for new products

--- alexa-site/src/app/admin/produtos/novo/VariationSection/VariationFieldInput.tsx
+++ alexa-site/src/app/admin/produtos/novo/VariationSection/VariationFieldInput.tsx
@@ -19,31 +19,31 @@ export default function VariationFieldInput({
     handleProductCustomPropertyChange,
 }: VariationFieldInputProps) {
     const { customProperties } = productVariationState;
-    if(variation in customProperties) {
-        return (
-            <div className="flex w-5/12">
-                <div className='flex flex-col gap-2 w-full'>
-                    <label className="text-xs" htmlFor={ variation }>{ variation.charAt(0).toUpperCase() + variation.slice(1) }</label>
-                    <input
-                        className="text-xs self-center px-3 py-2 border rounded-md w-5/6"
-                        id={ variation }
-                        name={ variation }
-                        type="text"
-                        value={ customProperties[variation as keyof typeof customProperties] || '' }
-                        onChange={ (e) => {
-                            setErrorMessage(undefined);
-                            handleProductDefaultPropertyChange('', 'barCode');
-                            handleProductDefaultPropertyChange('', 'sku');
-                            handleProductCustomPropertyChange(e.target.value, variation);
+    console.log('variation in customProperties', variation in customProperties);
+    console.log('variation', variation);
+    console.log('productVariationState', productVariationState);
+    return (
+        <div className="flex w-5/12">
+            <div className='flex flex-col gap-2 w-full'>
+                <label className="text-xs" htmlFor={ variation }>{ variation.charAt(0).toUpperCase() + variation.slice(1) }</label>
+                <input
+                    className="text-xs self-center px-3 py-2 border rounded-md w-5/6"
+                    id={ variation }
+                    name={ variation }
+                    type="text"
+                    value={ customProperties[variation as keyof typeof customProperties] || '' }
+                    onChange={ (e) => {
+                        setErrorMessage(undefined);
+                        handleProductDefaultPropertyChange('', 'barCode');
+                        handleProductDefaultPropertyChange('', 'sku');
+                        handleProductCustomPropertyChange(e.target.value, variation);
                             
-                        } }
-                        placeholder=''
-                    />
-                </div>
+                    } }
+                    placeholder=''
+                />
             </div>
-        );
-    } else {
-        return null;
-    }
+        </div>
+    );
+
 
 }
