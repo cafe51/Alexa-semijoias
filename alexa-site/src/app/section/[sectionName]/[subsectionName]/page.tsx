8bf2d8324213d3b918d46247228410cbed1fd8be
cafe51 <cafecafe51@hotmail.com>
2024-10-24 15:27:55 -0300

    fix: decoded section and subsection names

--- alexa-site/src/app/section/[sectionName]/[subsectionName]/page.tsx
+++ alexa-site/src/app/section/[sectionName]/[subsectionName]/page.tsx
@@ -9,8 +9,8 @@ export default function SubSection({ params: { sectionName, subsectionName } }: 
         // <p>{ `${sectionName}:${decodeURIComponent(subsectionName)}` }</p>
         <div className="min-h-screen bg-[#FAF9F6] text-[#333333] py-6 sm:py-8 px-3 sm:px-4 md:px-8" style={ { fontFamily: 'Montserrat, sans-serif' } }>
             <div className="max-w-7xl mx-auto">
-                <SectionPageTitle section={ sectionName } subsection={ decodeURIComponent(subsectionName) } />
-                <ProductsList sectionName={ sectionName } subsection={ `${sectionName}:${decodeURIComponent(subsectionName)}` }/>
+                <SectionPageTitle section={ decodeURIComponent(sectionName) } subsection={ decodeURIComponent(subsectionName) } />
+                <ProductsList sectionName={ sectionName } subsection={ `${decodeURIComponent(sectionName)}:${decodeURIComponent(subsectionName)}` }/>
             </div>
         </div>
     );
--- alexa-site/src/app/section/[sectionName]/page.tsx
+++ alexa-site/src/app/section/[sectionName]/page.tsx
@@ -1,14 +1,21 @@
 //app/section/[sectionName]/page.tsx
-
+'use client';
+import { useEffect } from 'react';
 import ProductsList from '../../components/ProductList/ProductsList';
 import SectionPageTitle from '../SectionPageTitle';
 
 export default function Section({ params }: { params: { sectionName: string } }) {
+
+    useEffect(() => {
+        console.log('XXXXXXXXXXXXXXX', params.sectionName);
+        console.log('YYYYYYYYYYYYY', decodeURIComponent(params.sectionName));
+    }, [params.sectionName]);
+    
     return (
         <div className="min-h-screen bg-[#FAF9F6] text-[#333333] py-6 sm:py-8 px-3 sm:px-4 md:px-8" style={ { fontFamily: 'Montserrat, sans-serif' } }>
             <div className="max-w-7xl mx-auto">
-                <SectionPageTitle section={ params.sectionName } />
-                <ProductsList sectionName={ params.sectionName } />
+                <SectionPageTitle section={ decodeURIComponent(params.sectionName) } />
+                <ProductsList sectionName={ decodeURIComponent(params.sectionName) } />
             </div>
         </div>
     );
