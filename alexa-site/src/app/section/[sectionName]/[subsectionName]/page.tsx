f696382f1bc70e1bdd16cd5e3de84f2b27134002
cafe51 <cafecafe51@hotmail.com>
2024-10-24 16:00:31 -0300

    fix: redirection on section and subsection page

--- alexa-site/src/app/section/[sectionName]/[subsectionName]/page.tsx
+++ alexa-site/src/app/section/[sectionName]/[subsectionName]/page.tsx
@@ -1,15 +1,37 @@
 //app/section/[subsectionName]/page.tsx
-
+'use client';
+import { useCollection } from '@/app/hooks/useCollection';
+import { useEffect, useState } from 'react';
 import ProductsList from '../../../components/ProductList/ProductsList';
 import SectionPageTitle from '../../SectionPageTitle';
+import { SectionType } from '@/app/utils/types';
+import { useRouter } from 'next/navigation';
 
 export default function SubSection({ params: { sectionName, subsectionName } }: { params: { subsectionName: string, sectionName: string } }) {
+    const { getAllDocuments: getAllSections } = useCollection<SectionType>('siteSections');
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        async function fetchDataFromFb() {
+            setLoading(true);
+            const sectionOnFirebase = await getAllSections([{ field: 'sectionName', operator: '==', value: decodeURIComponent(sectionName) }]);
+            console.log('SEEEEEEECTION', sectionOnFirebase);
+
+            if (sectionOnFirebase.length === 0 || !sectionOnFirebase[0].subsections?.includes(subsectionName)) {
+                router.push('/');
+            }
+            setLoading(false);
+
+            return sectionOnFirebase;
+        }
+        fetchDataFromFb();
+    }, []);
 
     return (
-        // <p>{ `${sectionName}:${decodeURIComponent(subsectionName)}` }</p>
         <div className="min-h-screen bg-[#FAF9F6] text-[#333333] py-6 sm:py-8 px-3 sm:px-4 md:px-8" style={ { fontFamily: 'Montserrat, sans-serif' } }>
             <div className="max-w-7xl mx-auto">
-                <SectionPageTitle section={ decodeURIComponent(sectionName) } subsection={ decodeURIComponent(subsectionName) } />
+                { !loading && <SectionPageTitle section={ decodeURIComponent(sectionName) } subsection={ decodeURIComponent(subsectionName) } /> }
                 <ProductsList sectionName={ decodeURIComponent(sectionName) } subsection={ `${decodeURIComponent(sectionName)}:${decodeURIComponent(subsectionName)}` }/>
             </div>
         </div>
--- alexa-site/src/app/section/[sectionName]/page.tsx
+++ alexa-site/src/app/section/[sectionName]/page.tsx
@@ -1,20 +1,38 @@
 //app/section/[sectionName]/page.tsx
 'use client';
-import { useEffect } from 'react';
+import { useCollection } from '@/app/hooks/useCollection';
+import { useEffect, useState } from 'react';
 import ProductsList from '../../components/ProductList/ProductsList';
 import SectionPageTitle from '../SectionPageTitle';
+import { SectionType } from '@/app/utils/types';
+import { useRouter } from 'next/navigation';
 
 export default function Section({ params }: { params: { sectionName: string } }) {
+    const { getAllDocuments: getAllSections } = useCollection<SectionType>('siteSections');
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
 
     useEffect(() => {
-        console.log('XXXXXXXXXXXXXXX', params.sectionName);
-        console.log('YYYYYYYYYYYYY', decodeURIComponent(params.sectionName));
-    }, [params.sectionName]);
-    
+        async function fetchDataFromFb() {
+            setLoading(true);
+            const sectionOnFirebase = await getAllSections([{ field: 'sectionName', operator: '==', value: decodeURIComponent(params.sectionName) }]);
+            console.log('SEEEEEEECTION', sectionOnFirebase);
+
+            if (sectionOnFirebase.length === 0) {
+                router.push('/');
+            }
+            setLoading(false);
+
+            return sectionOnFirebase;
+        }
+        fetchDataFromFb();
+    }, []);
+
+
     return (
         <div className="min-h-screen bg-[#FAF9F6] text-[#333333] py-6 sm:py-8 px-3 sm:px-4 md:px-8" style={ { fontFamily: 'Montserrat, sans-serif' } }>
             <div className="max-w-7xl mx-auto">
-                <SectionPageTitle section={ decodeURIComponent(params.sectionName) } />
+                { !loading && <SectionPageTitle section={ decodeURIComponent(params.sectionName) } /> }
                 <ProductsList sectionName={ decodeURIComponent(params.sectionName) } />
             </div>
         </div>
