cd290ac3988f6c4d02948e1ca5a2375956b2e5d7
cafe51 <cafecafe51@hotmail.com>
2024-10-25 13:56:58 -0300

    feat: link to sections on section cards on home page

--- alexa-site/src/app/components/homePage/SectionCard.tsx
+++ alexa-site/src/app/components/homePage/SectionCard.tsx
@@ -7,6 +7,7 @@ import { useEffect, useMemo, useState } from 'react';
 import { FilterOptionForUseSnapshot, ProductBundleType } from '@/app/utils/types';
 import { useCollection } from '@/app/hooks/useCollection';
 import { getImageUrlFromFirebaseProductDocument } from '@/app/utils/getImageUrlFromFirebaseProductDocument';
+import Link from 'next/link';
 
 interface SectionCardProps {
     section: string;
@@ -48,16 +49,19 @@ export default function SectionCard({ section }: SectionCardProps) {
 
     return (
         <Card className="overflow-hidden transition-transform duration-300 hover:scale-105 flex flex-col">
-            <CardContent className="p-0 relative aspect-square">
-                <Image
-                    src={ imageUrl }
-                    alt={ section }
-                    className="rounded-lg rounded-b-none object-cover scale-100"
-                    sizes="3000px"
-                    priority
-                    fill
-                />
+            <CardContent className="p-0 flex flex-col h-full">
+                <Link href={ `/section/${section}` } className='relative aspect-square'>
+                    <Image
+                        className="rounded-lg rounded-b-none object-cover scale-100"
+                        src={ imageUrl }
+                        alt={ section }
+                        sizes="3000px"
+                        priority
+                        fill
+                    />
+                </Link>
             </CardContent>
+
             <div className="p-4 text-center bg-[#C48B9F]">
                 <h3 className="font-semibold text-xl text-white">
                     { section.toUpperCase() }
