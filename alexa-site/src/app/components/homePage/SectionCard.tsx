f487b08059fd7c0d4e5de0bcf2a80c964c718ec9
cafe51 <cafecafe51@hotmail.com>
2024-10-25 17:34:50 -0300

    style: section card

--- alexa-site/src/app/components/homePage/SectionCard.tsx
+++ alexa-site/src/app/components/homePage/SectionCard.tsx
@@ -1,6 +1,5 @@
 // SectionCard.tsx
 'use client';
-
 import { Card, CardContent } from '@/components/ui/card';
 import Image from 'next/image';
 import { useEffect, useMemo, useState } from 'react';
@@ -25,13 +24,9 @@ export default function SectionCard({ section }: SectionCardProps) {
 
     useEffect(() => {
         async function fetchRandomProduct() {
-
             const randomSelectorValue = Math.floor(Math.random() * 99).toString().padStart(2, '0');
-
-            // Primeira consulta
             let randomProducts = await getAllProducts([...pedidosFiltrados, { field: 'randomIndex', operator: '>=', value: randomSelectorValue }], 4);
-
-            // Segunda consulta se necessário
+            
             if (randomProducts.length === 0) {
                 randomProducts = await getAllProducts([...pedidosFiltrados, { field: 'randomIndex', operator: '<', value: randomSelectorValue }], 4);
             }
@@ -43,16 +38,17 @@ export default function SectionCard({ section }: SectionCardProps) {
         fetchRandomProduct();
     }, [section, getAllProducts, pedidosFiltrados]);
 
-
-
     const imageUrl = getImageUrlFromFirebaseProductDocument(randomProduct);
 
+
+
     return (
-        <Card className="overflow-hidden transition-transform duration-300 hover:scale-105 flex flex-col">
-            <CardContent className="p-0 flex flex-col h-full">
+        <Card className="relative overflow-hidden transition-transform duration-300 hover:scale-105 flex flex-col">
+            <div className="absolute inset-0 bg-gradient-to-t from-[#C48B9F] from-0% via-[#C48B9F]/80 via-10% via-[#C48B9F]/40 via-25% to-transparent to-40% z-10" />
+            <CardContent className="p-0 flex flex-col h-full z-0">
                 <Link href={ `/section/${section}` } className='relative aspect-square'>
                     <Image
-                        className="rounded-lg rounded-b-none object-cover scale-100"
+                        className="rounded-lg rounded-b-none object-cover scale-100 z-0"
                         src={ imageUrl }
                         alt={ section }
                         sizes="3000px"
@@ -62,8 +58,23 @@ export default function SectionCard({ section }: SectionCardProps) {
                 </Link>
             </CardContent>
 
-            <div className="p-4 text-center bg-[#C48B9F]">
-                <h3 className="font-semibold text-xl text-white">
+            <div className="relative text-center h-16">
+                { /* Container para a imagem de fundo */ }
+                <div className="absolute inset-0 overflow-hidden">
+                    <div className="absolute inset-0 -top-[300px]">
+                        <Image
+                            src={ imageUrl }
+                            alt={ section }
+                            fill
+                            className="object-cover object-bottom"
+                            sizes="3000px"
+                            priority
+                        />
+                    </div>
+                </div>
+                
+                { /* Texto */ }
+                <h3 className="relative z-30 font-semibold text-xl sm:text-2xl text-white inline-block px-4 sm:px-6 py-2 sm:py-3">
                     { section.toUpperCase() }
                 </h3>
             </div>
