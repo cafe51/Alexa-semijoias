c73f89f1a9ed1f8a1ff4d46a56e87f79682f5719
cafe51 <cafecafe51@hotmail.com>
2024-11-04 16:14:46 -0300

    style: loading spin on cartIcon

--- alexa-site/src/app/components/header/CartIcon.tsx
+++ alexa-site/src/app/components/header/CartIcon.tsx
@@ -22,21 +22,16 @@ export default function CartIcon({ isMobile }: {isMobile: boolean}) {
         setLoadingIcon(false);
     }, [carrinho]);
 
-    // if(loadingIcon) return <p>X</p>;
-
     return (
         <Link className='relative w-full flex text-[#C48B9F]' href={ '/carrinho' }>
             <FiShoppingCart size={ isMobile ? 24 : 52 } data-testid='cartIcon'/>
             <span className={ `flex items-center justify-center text-white bg-[#C48B9F] rounded-full ${ isMobile ?'absolute -top-3 -right-3 w-5 h-5 text-xs' : 'w-7 h-7 text-lg'} ${ loadingIcon ? 'animate-spin' : ''}` }>
                 {
                 // carrinho && carrinho.map((items) => (Number(items.quantidade))).reduce((a, b) => a + b, 0)
-                    loadingIcon ? 'X' : cartQuantity
+                    loadingIcon ? <div className="animate-spin rounded-full h-2 w-2 border-b-2 border-white"></div> : cartQuantity
                 }
                 
             </span>
         </Link>
-        // cartQuantity > 0 && (
-        //     <Badge className="absolute -top-2 -right-2 bg-[#D4AF37] text-white">{ cartQuantity }</Badge>
-        // )
     );
 }
