879057914a869cce916d2b9b033ba8205d3babfc
cafe51 <cafecafe51@hotmail.com>
2024-11-21 12:12:56 -0300

    style: add hero banner logo

--- alexa-site/src/app/components/homePage/HeroSection.tsx
+++ alexa-site/src/app/components/homePage/HeroSection.tsx
@@ -5,8 +5,12 @@ import { useMediaQuery } from 'react-responsive';
 import heroBannerSmall from '@/../public/heroBannerSmall.png';
 import heroBannerLarge from '@/../public/heroBannerLarge.png';
 import heroBannerMedium from '@/../public/heroBannerMedium.png';
+import bigHeroLogo from '@/../public/bigHeroLogo2.png';
+// import mediumHeroLogo from '@/../public/mediumHeroLogo.png';
+// import smallHeroLogo from '@/../public/smallHeroLogo.png';
 
-import { Diamond } from 'lucide-react';
+
+// import { Diamond } from 'lucide-react';
 
 export default function HeroSection() {
     // Hook para verificar tamanho da tela
@@ -20,6 +24,13 @@ export default function HeroSection() {
         : isMediumScreen
             ? heroBannerMedium
             : heroBannerSmall;
+
+
+    // const heroLogoImage = isLargeScreen
+    //     ? bigHeroLogo
+    //     : isMediumScreen
+    //         ? mediumHeroLogo
+    //         : smallHeroLogo;
 
     return (
         <section className="relative flex flex-col justify-center ">
@@ -39,10 +50,27 @@ export default function HeroSection() {
             <div className="absolute w-full h-full flex flex-col items-center justify-center pt-32"> 
                 <div className="flex flex-col items-center justify-center text-center w-full">
 
-                    <Diamond className="text-white" size={ isLargeScreen ? 80 : 60 } />
-                    <span className={ `text-white  ${isLargeScreen ? 'text-7xl' : isMediumScreen ? 'text-6xl' : 'text-5xl'}` }>
-                    ALEXA
-                    </span>
+                    {
+                        //         <>
+                        //             <Diamond className="text-white" size={ isLargeScreen ? 80 : 60 } />
+                        //             <span className={ `text-white  ${isLargeScreen ? 'text-7xl' : isMediumScreen ? 'text-6xl' : 'text-5xl'}` }>
+                        // ALEXA
+                        //             </span>
+                        //         </>   
+                        <div className="pt-8 flex flex-col h-full z-10">
+                            <div className='relative aspect-square h-36 md:h-72 lg:h-96'>
+                                <Image
+                                    className='rounded-lg rounded-b-none object-cover scale-100'
+                                    src={ bigHeroLogo }
+                                    alt="Hero Banner"
+                                    priority
+                                    sizes="2200px"
+                                    fill
+                                />
+                            </div>
+                        </div>
+      
+                    }
                 </div>
             </div>
         </section>
