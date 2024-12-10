ebeb44287abfdb0a0420912bcac4a771e0339984
cafe51 <cafecafe51@hotmail.com>
2024-12-10 12:52:26 -0300

    fix: heigth of the screen when loading component

--- alexa-site/src/app/components/LoadingIndicator.tsx
+++ alexa-site/src/app/components/LoadingIndicator.tsx
@@ -1,5 +1,5 @@
 const LoadingIndicator = () => (
-    <div className="flex justify-center items-center h-64">
+    <div className="flex justify-center items-center h-64 md:h-4/6">
         <div className="animate-spin rounded-full h-10 w-10 border-b-4 border-[#F8C3D3]"></div>
         <p className="ml-2">Carregando...</p>
     </div>
