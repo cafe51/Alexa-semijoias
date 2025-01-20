0ac0f4c3c1aa2600db0c9f06afc9d8d7d640784a
cafe51 <cafecafe51@hotmail.com>
2025-01-20 19:07:42 -0300

    feat: free shipping section

--- alexa-site/src/app/checkout/DeliveryPriceSection/FreeShippingSection.tsx
+++ alexa-site/src/app/checkout/DeliveryPriceSection/FreeShippingSection.tsx
@@ -2,17 +2,26 @@
 
 export default function FreeShippingSection({ precoFaltanteEmPorcentagem, precoFaltanteParaFreteGratis }: { precoFaltanteEmPorcentagem: string, precoFaltanteParaFreteGratis: number }) {
     return (
-        <div className="p-4 border-dashed border-2 border-green-500 bg-green-50 rounded-md mb-4">
-            <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4">
-                <div className="bg-green-500 h-2.5 rounded-full" style={ { width: precoFaltanteEmPorcentagem } }></div>
-            </div>
-            <p className="text-green-600 text-sm mb-2">
-          Quase lá, adicione mais <span className="font-bold">{ 'R$ ' + precoFaltanteParaFreteGratis.toFixed(2) }</span> em produtos para ganhar <span className="font-bold">FRETE GRÁTIS</span>.
-            </p>
-            <button className="bg-green-100 text-green-700 border border-green-500 px-4 py-2 rounded-md text-sm">
-          Adicionar mais produtos
-            </button>
-        </div>
+        // text-[#D4AF37]
+        precoFaltanteParaFreteGratis > 0 ?
+            (
+                <div className="p-4 border-dashed border-2 border-[#D4AF37]/50 bg-[#D4AF37]/2 rounded-md mb-4">
+                    <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4">
+                        <div className="bg-[#D4AF37]/80 h-2.5 rounded-full" style={ { width: precoFaltanteEmPorcentagem } }></div>
+                    </div>
+                    <p className="text-[#D4AF37] text-base mb-2">
+          Quase lá, adicione mais <span className="font-bold">{ 'R$ ' + precoFaltanteParaFreteGratis.toFixed(2) }</span> em produtos para ganhar <span className="font-bold text-lg">FRETE GRÁTIS</span>.
+                    </p>
+                    <button className="bg-[#D4AF37]/5 text-[#D4AF37] border border-[#D4AF37]/50 px-4 py-2 rounded-md text-sm">Adicionar mais produtos</button>
+                </div>
+            )
+            :
+            (
+                <div className="p-4 border-dashed border-2 border-[#D4AF37]/50 bg-[#D4AF37]/5 rounded-md mb-4">
+                    <p className="text-[#D4AF37] text-xl mb-2">Parabéns!</p>
+                    <p className="text-[#D4AF37] text-sm mb-2">Você ganhou uma opção de <span className="font-bold">FRETE GRÁTIS</span> para esta compra.</p>
+                </div>
+            )
+        
     );
 }
-  
