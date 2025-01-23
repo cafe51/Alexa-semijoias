04fcdb7ae46d41b7a9722e3094f446994a4c6d8a
cafe51 <cafecafe51@hotmail.com>
2025-01-23 10:03:11 -0300

    feat: meta pixel script on head

--- alexa-site/src/app/layout.tsx
+++ alexa-site/src/app/layout.tsx
@@ -24,6 +24,33 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 <meta name="facebook-domain-verification" content="k9qc45dlpt6m4sxg6a3qmin2rmabmh" />
+                { /* Meta Pixel Code */ }
+                <script
+                    dangerouslySetInnerHTML={ {
+                        __html: `
+                            !function(f,b,e,v,n,t,s)
+                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script',
+                            'https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '574789162040735');
+                            fbq('track', 'PageView');
+                        `,
+                    } }
+                />
+                <noscript>
+                    <img 
+                        height="1" 
+                        width="1" 
+                        style={ { display: 'none' } }
+                        src="https://www.facebook.com/tr?id=574789162040735&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
+                { /* End Meta Pixel Code */ }
             </head>
             <body className={ `${inter.className} min-h-screen bg-[#FAF9F6]` }>
                 <AuthContextProvider>
