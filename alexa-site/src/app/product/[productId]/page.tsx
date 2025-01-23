934ceb0bc15fcc396cdf429345ef9abf5110f2b3
cafe51 <cafecafe51@hotmail.com>
2025-01-23 15:27:52 -0300

    type website again

--- alexa-site/src/app/product/[productId]/page.tsx
+++ alexa-site/src/app/product/[productId]/page.tsx
@@ -33,6 +33,7 @@ export async function generateMetadata({ params }: { params: { productId: string
                         alt: productData.name,
                     },
                 ],
+                type: 'website',
                 siteName: 'Alexa Semijoias',
             },
             other: {
