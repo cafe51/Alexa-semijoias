f3d018bb3e7b97604ac539cbbd2bebfc8716a266
cafe51 <cafecafe51@hotmail.com>
2025-02-06 11:06:58 -0300

    fix: IPv6 instead of IPv4 now when send to meta by api conversion

--- alexa-site/src/app/utils/meta-conversions/service.ts
+++ alexa-site/src/app/utils/meta-conversions/service.ts
@@ -27,7 +27,7 @@ export class MetaConversionsService {
 
     private async getClientIp(): Promise<string | undefined> {
         try {
-            const response = await fetch('https://api.ipify.org?format=json');
+            const response = await fetch('https://api64.ipify.org?format=json');
             const data = await response.json();
             return data.ip;
         } catch (error) {
