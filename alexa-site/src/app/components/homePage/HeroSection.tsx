ba974d7f586f92beebf925b4bfd8879f1d7f611f
cafe51 <cafecafe51@hotmail.com>
2025-02-07 19:53:34 -0300

    fix: hero section component for better LCP

--- alexa-site/src/app/components/homePage/HeroSection.tsx
+++ alexa-site/src/app/components/homePage/HeroSection.tsx
@@ -1,12 +1,10 @@
 // src/app/components/homePage/HeroSection.tsx
-'use client';
-
+// Removemos o 'use client' porque esse componente é estático e não interage com o estado
 import Image from 'next/image';
 import heroBannerLarge from '@/../public/heroBannerLarge.webp';
 import bigHeroLogo from '@/../public/bigHeroLogo2.png';
 
 export default function HeroSection() {
-
     return (
         <section className="relative w-full">
             { /* Container principal com altura fixa para desktop */ }
@@ -14,22 +12,23 @@ export default function HeroSection() {
                 <Image
                     className="object-cover"
                     src={ heroBannerLarge }
-                    title='Banner Principal Alexa Semijoias'
+                    title="Banner Principal Alexa Semijoias"
                     alt="Banner Principal Alexa Semijoias"
                     priority
-                    sizes='200x'
-                    quality={ 80 }
+                    // Usamos "100vw" para informar que a imagem ocupa toda a largura da viewport
+                    sizes="100vw"
+                    quality={ 80 } // Se possível, teste reduzir um pouco esse valor (ex: 75) sem comprometer a qualidade visual
                     fill
                     placeholder="blur"
                     blurDataURL={ heroBannerLarge.blurDataURL }
                 />
-                
+        
                 { /* Container do logo centralizado */ }
                 <div className="absolute inset-0 flex items-center justify-center">
                     <div className="relative w-[90%] md:w-[60%] lg:w-[50%] aspect-[3/1]">
                         <Image
                             className="object-contain"
-                            title='Logo Alexa Semijoias'
+                            title="Logo Alexa Semijoias"
                             src={ bigHeroLogo }
                             alt="Logo Alexa Semijoias"
                             priority
