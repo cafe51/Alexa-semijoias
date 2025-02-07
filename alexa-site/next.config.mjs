4454002b588eef6aee516b8af54192e5f32f4dcd
cafe51 <cafecafe51@hotmail.com>
2025-02-07 16:53:01 -0300

    feat: url redirections for better SEO

--- alexa-site/next.config.mjs
+++ alexa-site/next.config.mjs
@@ -40,6 +40,36 @@ const nextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [
+            {
+            // Redireciona requisições feitas para a versão non-www para a versão www.
+            // Quando o host for "alexasemijoias.com.br", redireciona para "https://www.alexasemijoias.com.br"
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'alexasemijoias.com.br',
+                    },
+                ],
+                destination: 'https://www.alexasemijoias.com.br/:path*',
+                permanent: true,
+            },
+            {
+            // Redireciona "/index.html" para "/"
+                source: '/index.html',
+                destination: '/',
+                permanent: true,
+            },
+            {
+            // Redireciona "/index.php" para "/"
+                source: '/index.php',
+                destination: '/',
+                permanent: true,
+            },
+        ];
+    },
+    
 };
 
 export default nextConfig;
