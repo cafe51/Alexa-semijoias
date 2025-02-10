6803211bc911df2d786cabf54d34e193e5b51577
cafe51 <cafecafe51@hotmail.com>
2025-02-10 12:16:25 -0300

    fix: component import

--- alexa-site/src/app/search/[searchTerm]/page.tsx
+++ alexa-site/src/app/search/[searchTerm]/page.tsx
@@ -1,14 +1,14 @@
 // src/app/search/[searchTerm]/page.tsx
 
 import PageContainer from '@/app/components/PageContainer';
-import ProductsList from '@/app/components/ProductList/ProductsList';
+import ProductsListClient from '@/app/components/ProductList/ProductsListClient';
 
 export default function Search({ params }: { params: { searchTerm: string } }) {
     return (
         <PageContainer>
             <>
                 <h1 className="text-2xl font-semibold mb-6">Resultados para: { decodeURIComponent(params.searchTerm) }</h1>
-                <ProductsList searchTerm={ decodeURIComponent(params.searchTerm) } />
+                <ProductsListClient searchTerm={ decodeURIComponent(params.searchTerm) } />
             </>
         </PageContainer>
     );
