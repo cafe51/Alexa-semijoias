6a7ac24d3087f6e811a23e94e4848ac408de4f7c
cafe51 <cafecafe51@hotmail.com>
2025-02-21 10:53:26 -0300

    style: new style on multiselectmodal

--- alexa-site/src/app/admin/produtos/components/MultiSelectModal.tsx
+++ alexa-site/src/app/admin/produtos/components/MultiSelectModal.tsx
@@ -1,5 +1,6 @@
 // src/app/components/MultiSelectModal.tsx
 'use client';
+import { Settings } from 'lucide-react';
 import React from 'react';
 import { FiX } from 'react-icons/fi';
 
@@ -18,25 +19,24 @@ export default function MultiSelectModal({ isOpen, selectedCount, onCancel, onDe
         <div className={ `fixed inset-x-0 bottom-0 z-50 transform transition-transform duration-300 ${isOpen ? 'translate-y-0' : 'translate-y-full'}` }>
             <div className="bg-white p-4 rounded-t-md w-full shadow-[0_-4px_8px_rgba(0,0,0,0.5)]">
                 <div className="flex justify-between items-center">
-                    <h3 className="font-bold text-lg">Opções de Multiseleção</h3>
+                    <h3 className="font-bold text-lg text-center">{ selectedCount } { selectedCount > 1 ? 'itens selecionados' : 'item selecionado' }</h3>
                     <button onClick={ onCancel }>
                         <FiX size={ 24 } />
                     </button>
                 </div>
-                <div className="mt-4">
-                    <p>{ selectedCount } item(s) selecionado(s)</p>
-                </div>
-                <div className="flex flex-col  justify-end gap-4 mt-4">
-                    <button onClick={ onSelectAll } className="px-4 py-2 bg-blue-500 text-white rounded-md">
-                        Selecionar Todos
+                <div className="flex flex-col justify-end gap-4 mt-4">
+                    <div className="flex justify-between  mt-4">
+                        <button onClick={ onSelectAll } className="flex-1 py-2 px-4 bg-gray-100 text-gray-800 rounded-lg hover:bg-gray-200 transition-colors">
+                            Todos
+                        </button>
+                        <button onClick={ onDelete } className="flex-1 py-2 px-4 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors" disabled={ isDeleting }>
+                            { isDeleting ? 'Deletando...' : 'Deletar' }
+                        </button>
+                    </div>
+                    <button onClick={ onModify } className="w-full py-2 px-4 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition-colors flex items-center justify-center">
+                        <Settings size={ 20 } className="mr-2" />Opções avançadas
                     </button>
-                    <button onClick={ onModify } className="px-4 py-2 bg-green-500 text-white rounded-md">
-                        Modificar Selecionados
-                    </button>
-                    <button onClick={ onCancel } className="px-4 py-2 bg-gray-300 rounded-md">Cancelar</button>
-                    <button onClick={ onDelete } className="px-4 py-2 bg-red-500 text-white rounded-md" disabled={ isDeleting }>
-                        { isDeleting ? 'Deletando...' : 'Deletar Selecionados' }
-                    </button>
+
                 </div>
             </div>
         </div>
