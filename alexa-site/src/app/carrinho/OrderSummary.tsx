2439cc2ff57835459f1bd5dd36a42a7893cfeedd
cafe51 <cafecafe51@hotmail.com>
2025-02-24 10:19:27 -0300

    feat: freeshipping now shows on cart page

--- alexa-site/src/app/carrinho/OrderSummary.tsx
+++ alexa-site/src/app/carrinho/OrderSummary.tsx
@@ -19,37 +19,38 @@ export default function OrderSummary({
 }: { 
     subtotal: number;
     shipping: number | null;
-    onSelectShipping: (optionId: string) => void;
+    onSelectShipping: (optionPrice: string) => void;
     onCheckout: () => void;
-    carrinho: (ProductCartType & FireBaseDocument)[] | ProductCartType[]
+    carrinho: (ProductCartType & FireBaseDocument)[] | ProductCartType[];
     couponDiscount: number | 'freteGratis';
 }) {
     const totalItems = useMemo(() => {
         if (!carrinho || carrinho.length === 0) return 0;
-        return carrinho.map((items) => (Number(items.quantidade))).reduce((a, b) => a + b, 0);
+        return carrinho.map((items) => Number(items.quantidade)).reduce((a, b) => a + b, 0);
     }, [carrinho]);
 
     const couponDiscountIsFreeShipping = useMemo(() => {
-        return !!couponDiscount && (couponDiscount === 'freteGratis'); 
+        return !!couponDiscount && couponDiscount === 'freteGratis'; 
     }, [couponDiscount]);
 
     const couponDiscountValue = useMemo(() => {
-        return  !couponDiscountIsFreeShipping && !!couponDiscount && (couponDiscount !== 'freteGratis') ? couponDiscount : 0;
+        return !couponDiscountIsFreeShipping && !!couponDiscount && couponDiscount !== 'freteGratis' ? couponDiscount : 0;
     }, [couponDiscount, couponDiscountIsFreeShipping]);
 
     const totalValue = useMemo(() => {
         return (
-            (subtotal - (couponDiscountIsFreeShipping ? 0 : couponDiscountValue)) + (couponDiscountIsFreeShipping ? 0 : (shipping || 0)) 
+            (subtotal - (couponDiscountIsFreeShipping ? 0 : couponDiscountValue)) + 
+            (couponDiscountIsFreeShipping ? 0 : (shipping || 0))
         );
     }, [subtotal, shipping, couponDiscountIsFreeShipping, couponDiscountValue]);
-
-
 
     return (
         <div className="w-full md:max-w-2xl md:mx-auto flex-grow">
             <Card className="w-full shadow-md shadow-[#C48B9F]">
                 <CardContent className="p-6 md:p-8">
-                    <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-4 md:mb-6">Resumo do Pedido</h2>
+                    <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-4 md:mb-6">
+                        Resumo do Pedido
+                    </h2>
                     <div className="space-y-2 md:space-y-4 mb-4 md:mb-6">
                         <div className="flex justify-between">
                             <span className="text-base md:text-lg lg:text-xl">Subtotal:</span>
@@ -58,34 +59,39 @@ export default function OrderSummary({
 
                         { !couponDiscountIsFreeShipping && !!couponDiscount && (
                             <div className="flex justify-between items-center mb-2">
-                                <span className=" md:text-xl font-bold">Desconto:</span>
-                                <span className=" font-medium md:text-xl">- { formatPrice(couponDiscountValue) }</span>
+                                <span className="md:text-xl font-bold">Desconto:</span>
+                                <span className="font-medium md:text-xl">- { formatPrice(couponDiscountValue) }</span>
                             </div>
                         ) }
 
                         { couponDiscountIsFreeShipping && (
                             <div className="flex justify-between items-center mb-2">
-                                <span className=" md:text-xl font-bold">Desconto:</span>
-                                <span className=" font-medium md:text-xl">Frete Grátis</span>
+                                <span className="md:text-xl font-bold">Desconto:</span>
+                                <span className="font-medium md:text-xl">Frete Grátis</span>
                             </div>
                         ) }
 
-                        {
-                            !couponDiscountIsFreeShipping &&(
-                                <div className="flex justify-between items-center">
-                                    <span className="text-base md:text-lg lg:text-xl">Frete:</span>
-                                    <ShippingCalculator onSelectShipping={ onSelectShipping } selectedShipping={ shipping }/>
-                                </div>
-                            )
-                        }
+                        { !couponDiscountIsFreeShipping && (
+                            <div className="flex justify-between items-center">
+                                <span className="text-base md:text-lg lg:text-xl">Frete:</span>
+                                <ShippingCalculator 
+                                    onSelectShipping={ onSelectShipping } 
+                                    selectedShipping={ shipping } 
+                                    cartPrice={ subtotal }
+                                    couponDiscount={ couponDiscount }
+                                />
+                            </div>
+                        ) }
                     </div>
                     <Separator className="my-4 md:my-6" />
                     <div className="flex justify-between items-center mb-6 md:mb-8">
                         <span className="text-lg md:text-xl lg:text-2xl font-semibold">Total:</span>
                         <span className="text-lg md:text-xl lg:text-2xl font-semibold">{ formatPrice(totalValue) }</span>
                     </div>
-                    <div className='flex flex-col text-end w-full'>
-                        <p className="text-sm md:text-base lg:text-lg">Em até 6x <span className='font-bold'>{ formatPrice(totalValue / 6) }</span> sem juros</p>
+                    <div className="flex flex-col text-end w-full">
+                        <p className="text-sm md:text-base lg:text-lg">
+                            Em até 6x <span className="font-bold">{ formatPrice(totalValue / 6) }</span> sem juros
+                        </p>
                     </div>
                     <Button 
                         className="w-full bg-[#D4AF37] hover:bg-[#C48B9F] text-white text-base md:text-lg lg:text-xl py-2 md:py-6 mt-4 md:mt-6"
@@ -95,8 +101,8 @@ export default function OrderSummary({
                                 value: totalValue,
                                 num_items: totalItems,
                                 content_type: 'product',
-                                content_ids: carrinho?.map(item => item.skuId),
-                                contents: carrinho?.map(item => ({
+                                content_ids: carrinho.map(item => item.skuId),
+                                contents: carrinho.map(item => ({
                                     id: item.skuId,
                                     quantity: item.quantidade,
                                 })),
--- alexa-site/src/app/carrinho/ShippingCalculator.tsx
+++ alexa-site/src/app/carrinho/ShippingCalculator.tsx
@@ -1,5 +1,5 @@
 // src/app/carrinho/ShippingCalculator.tsx
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { ShippingOptionType } from '../utils/types';
 import { formatPrice } from '../utils/formatPrice';
 import fetchAddressFromCEP from '../utils/fetchAddressFromCEP';
@@ -7,21 +7,36 @@ import getShippingOptions from '../utils/getShippingOptions';
 import { formatCep } from '../utils/formatCep';
 import InputField from '../checkout/AddressSection/InputField';
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
-
+import FreeShippingSection from '../checkout/DeliveryPriceSection/FreeShippingSection';
 
 interface ShippingCalculatorProps {
-    onSelectShipping: (optionId: string) => void;
-    selectedShipping: number | null;
+  onSelectShipping: (optionPrice: string) => void;
+  selectedShipping: number | null;
+  cartPrice: number;
+  couponDiscount: number | 'freteGratis';
 }
 
-export default function ShippingCalculator({ onSelectShipping, selectedShipping }: ShippingCalculatorProps) {
+export default function ShippingCalculator({
+    onSelectShipping,
+    selectedShipping,
+    cartPrice,
+    couponDiscount,
+}: ShippingCalculatorProps) {
     const [cep, setCep] = useState('');
-    const [shippingOptions, setShippingOptions] = useState<ShippingOptionType[] | []>([]);
+    const [shippingOptions, setShippingOptions] = useState<ShippingOptionType[]>([]);
     const [showOptions, setShowOptions] = useState(false);
-    const [tempSelectedShipping, setTempSelectedShipping] = useState<string | null>(selectedShipping ? selectedShipping.toString() : null);
+    // Armazena o valor selecionado de forma única: "id||price"
+    const [tempSelectedShipping, setTempSelectedShipping] = useState<string | null>(null);
     const [isOpen, setIsOpen] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
@@ -36,15 +51,15 @@ export default function ShippingCalculator({ onSelectShipping, selectedShipping 
         setError(null);
         setIsLoading(true);
         setShowOptions(false);
-        
+
         try {
             const fetchedAddress = await fetchAddressFromCEP(cep);
-            
+
             if (fetchedAddress.erro) {
                 setError('CEP não encontrado. Por favor, verifique o CEP informado.');
                 return;
             }
-            
+
             const options = getShippingOptions(fetchedAddress.localidade, fetchedAddress.uf);
             setShippingOptions(options);
             setShowOptions(true);
@@ -57,24 +72,100 @@ export default function ShippingCalculator({ onSelectShipping, selectedShipping 
 
     const handleConfirm = () => {
         if (tempSelectedShipping) {
-            const selectedOption = shippingOptions.find(option => option.price.toString() === tempSelectedShipping);
+            const [selectedId] = tempSelectedShipping.split('||');
+            let selectedOption = shippingOptions.find(option => option.id === selectedId);
             if (selectedOption) {
-                onSelectShipping(selectedOption.price.toString()); // Passando o preço como string
+                // Determina a opção mais barata e a meta para frete grátis
+                const cheapestOption = shippingOptions.reduce(
+                    (prev, current) => (prev.price < current.price ? prev : current),
+                    shippingOptions[0],
+                );
+                let valorMinimoParaFreteGratis: number;
+                if (cheapestOption.price <= 40) {
+                    valorMinimoParaFreteGratis = 200;
+                } else if (cheapestOption.price > 40 && cheapestOption.price <= 70) {
+                    valorMinimoParaFreteGratis = 300;
+                } else if (cheapestOption.price > 70) {
+                    valorMinimoParaFreteGratis = 350;
+                } else {
+                    throw new Error('Preço inválido');
+                }
+                const discountValue = typeof couponDiscount === 'number' ? couponDiscount : 0;
+                const precoFaltanteParaFreteGratis =
+          couponDiscount === 'freteGratis'
+              ? 0
+              : valorMinimoParaFreteGratis - cartPrice + discountValue;
+                // Se a opção selecionada for a mais barata e a meta estiver atingida, define o frete como gratuito
+                if (selectedOption.name === cheapestOption.name && precoFaltanteParaFreteGratis <= 0) {
+                    selectedOption = { ...selectedOption, price: 0 };
+                }
+                onSelectShipping(selectedOption.price.toString());
             }
         }
         setIsOpen(false);
+        // Limpa a seleção temporária para próximas aberturas
+        setTempSelectedShipping(null);
     };
+
+    // Calcula os dados para exibição da barra de frete grátis e identifica a opção mais barata
+    const freeShippingData = useMemo(() => {
+        if (shippingOptions.length === 0) {
+            return { precoFaltanteParaFreteGratis: 0, precoFaltanteEmPorcentagem: '0%', cheapestOption: null };
+        }
+        const cheapestOption = shippingOptions.reduce(
+            (prev, current) => (prev.price < current.price ? prev : current),
+            shippingOptions[0],
+        );
+        let valorMinimoParaFreteGratis: number;
+        if (cheapestOption.price <= 40) {
+            valorMinimoParaFreteGratis = 200;
+        } else if (cheapestOption.price > 40 && cheapestOption.price <= 70) {
+            valorMinimoParaFreteGratis = 300;
+        } else if (cheapestOption.price > 70) {
+            valorMinimoParaFreteGratis = 350;
+        } else {
+            throw new Error('Preço inválido');
+        }
+        const discountValue = typeof couponDiscount === 'number' ? couponDiscount : 0;
+        const precoFaltanteParaFreteGratis =
+      couponDiscount === 'freteGratis'
+          ? 0
+          : valorMinimoParaFreteGratis - cartPrice + discountValue;
+        const percentage = Math.min(100, (cartPrice / valorMinimoParaFreteGratis) * 100);
+        const precoFaltanteEmPorcentagem = percentage + '%';
+        return { precoFaltanteParaFreteGratis, precoFaltanteEmPorcentagem, cheapestOption };
+    }, [shippingOptions, cartPrice, couponDiscount]);
+
+    // Flag para indicar se o frete selecionado é gratuito
+    const isFreeShipping = selectedShipping === 0 && freeShippingData.cheapestOption;
 
     return (
         <Dialog open={ isOpen } onOpenChange={ setIsOpen }>
             <DialogTrigger asChild>
-                { selectedShipping ? (
-                    <Button variant="link" className="p-0 h-auto text-[#C48B9F] hover:text-[#D4AF37] underline text-sm md:text-base lg:text-lg">
-                        { formatPrice(shippingOptions.find(option => option.price === selectedShipping)?.price || 0) }
+                { selectedShipping !== null ? (
+                    <Button variant="link" className="p-0 h-auto">
+                        { isFreeShipping ? (
+                            <div className="flex flex-col items-end">
+                                <div className="flex items-center">
+                                    <span className="line-through text-sm text-gray-400">
+                                        { formatPrice(freeShippingData.cheapestOption!.price) }
+                                    </span>
+                                    <span className="text-[#D4AF37] text-base font-bold ml-2">GRÁTIS</span>
+                                </div>
+
+                            </div>
+                        ) : (
+                            <span className="text-[#C48B9F] hover:text-[#D4AF37] underline text-sm md:text-base lg:text-lg">
+                                { formatPrice(selectedShipping) }
+                            </span>
+                        ) }
                     </Button>
                 ) : (
-                    <Button variant="link" className="p-0 h-auto text-[#C48B9F] hover:text-[#D4AF37] text-sm md:text-base lg:text-lg">
-                        Calcular frete
+                    <Button
+                        variant="link"
+                        className="p-0 h-auto text-[#C48B9F] hover:text-[#D4AF37] text-sm md:text-base lg:text-lg"
+                    >
+            Calcular frete
                     </Button>
                 ) }
             </DialogTrigger>
@@ -84,7 +175,9 @@ export default function ShippingCalculator({ onSelectShipping, selectedShipping 
                 </DialogHeader>
                 <form onSubmit={ handleCalculate } className="space-y-4 md:space-y-6">
                     <div className="flex items-center space-x-2 md:space-x-4">
-                        <Label htmlFor="cep" className="text-sm md:text-base lg:text-lg">CEP:</Label>
+                        <Label htmlFor="cep" className="text-sm md:text-base lg:text-lg">
+              CEP:
+                        </Label>
                         <InputField
                             id="cep"
                             value={ cep }
@@ -93,52 +186,84 @@ export default function ShippingCalculator({ onSelectShipping, selectedShipping 
                             label="CEP"
                             readOnly={ false }
                         />
-                        <Button 
-                            type="submit" 
-                            className="text-sm md:text-base lg:text-lg"
-                            disabled={ isLoading }
-                        >
+                        <Button type="submit" className="text-sm md:text-base lg:text-lg" disabled={ isLoading }>
                             { isLoading ? 'Calculando...' : 'Calcular' }
                         </Button>
                     </div>
                 </form>
                 { error && (
-                    <div className="mt-4 text-red-500 text-sm md:text-base">
-                        { error }
-                    </div>
+                    <div className="mt-4 text-red-500 text-sm md:text-base">{ error }</div>
                 ) }
-                { !error && showOptions && (
-                    <RadioGroup
-                        value={ tempSelectedShipping ? tempSelectedShipping : undefined }
-                        onValueChange={ setTempSelectedShipping }
-                        className="mt-4 md:mt-6 space-y-2 md:space-y-3"
-                    >
-                        { shippingOptions.map((option) => (
-                            <div
-                                key={ option.id }
-                                className="flex items-center justify-between space-x-2 md:space-x-4 border p-2 md:p-3 lg:p-4 rounded cursor-pointer hover:bg-gray-100"
-                                onClick={ () => setTempSelectedShipping(option.price.toString()) }
-                            >
-                                <div className="flex items-center flex-1">
-                                    <RadioGroupItem value={ option.price.toString() } id={ option.id } />
-                                    <Label htmlFor={ option.id } className="ml-2 md:ml-3 flex-1 cursor-pointer text-sm md:text-base lg:text-lg">{ option.name }</Label>
-                                </div>
-                                <div className="text-right">
-                                    <p className="text-sm md:text-base lg:text-lg">{ formatPrice(option.price) }</p>
-                                    <p className="text-xs md:text-sm lg:text-base text-gray-500">Entrega em até { option.days } dias úteis</p>
-                                </div>
-                            </div>
-                        )) }
-                    </RadioGroup>
+                { (!error && showOptions) && (
+                    <>
+                        { shippingOptions.length > 0 && (
+                            <FreeShippingSection
+                                precoFaltanteEmPorcentagem={ freeShippingData.precoFaltanteEmPorcentagem }
+                                precoFaltanteParaFreteGratis={ freeShippingData.precoFaltanteParaFreteGratis }
+                            />
+                        ) }
+                        <RadioGroup
+                            value={ tempSelectedShipping ?? undefined }
+                            onValueChange={ setTempSelectedShipping }
+                            className="mt-4 md:mt-6 space-y-2 md:space-y-3"
+                        >
+                            { shippingOptions.map((option) => {
+                                const uniqueValue = `${option.id}||${option.price}`;
+                                const isCheapestAndFree =
+                  freeShippingData.cheapestOption &&
+                  option.id === freeShippingData.cheapestOption.id &&
+                  freeShippingData.precoFaltanteParaFreteGratis <= 0;
+                                return (
+                                    <div
+                                        key={ option.id }
+                                        className="flex items-center justify-between space-x-2 md:space-x-4 border p-2 md:p-3 lg:p-4 rounded cursor-pointer hover:bg-gray-100"
+                                        onClick={ () => setTempSelectedShipping(uniqueValue) }
+                                    >
+                                        <div className="flex items-center flex-1">
+                                            <RadioGroupItem value={ uniqueValue } id={ option.id } />
+                                            <Label
+                                                htmlFor={ option.id }
+                                                className="ml-2 md:ml-3 flex-1 cursor-pointer text-sm md:text-base lg:text-lg"
+                                            >
+                                                { option.name }
+                                            </Label>
+                                        </div>
+                                        <div className="text-right">
+                                            { isCheapestAndFree ? (
+                                                <div className="flex flex-col items-end">
+                                                    <div className="flex items-center">
+                                                        <span className="line-through text-sm text-gray-400">
+                                                            { formatPrice(option.price) }
+                                                        </span>
+                                                        <span className="text-[#D4AF37] text-base font-bold ml-2">GRÁTIS</span>
+                                                    </div>
+                                                    <p className="text-xs md:text-sm lg:text-base text-gray-500">
+                            Entrega em até { option.days } dias úteis
+                                                    </p>
+                                                </div>
+                                            ) : (
+                                                <div className="flex flex-col items-end">
+                                                    <p className="text-sm md:text-base lg:text-lg">{ formatPrice(option.price) }</p>
+                                                    <p className="text-xs md:text-sm lg:text-base text-gray-500">
+                            Entrega em até { option.days } dias úteis
+                                                    </p>
+                                                </div>
+                                            ) }
+                                        </div>
+                                    </div>
+                                );
+                            }) }
+                        </RadioGroup>
+                    </>
                 ) }
                 <DialogFooter>
-                    <Button 
-                        onClick={ handleConfirm } 
-                        disabled={ !tempSelectedShipping } 
+                    <Button
+                        onClick={ handleConfirm }
+                        disabled={ !tempSelectedShipping }
                         aria-disabled={ !tempSelectedShipping }
                         className="text-sm md:text-base lg:text-lg"
                     >
-                        Confirmar
+            Confirmar
                     </Button>
                 </DialogFooter>
             </DialogContent>
