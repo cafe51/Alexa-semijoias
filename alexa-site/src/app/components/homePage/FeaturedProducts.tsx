5cc648dd5cb694c7e4076a666af7dcf832ba7f8c
cafe51 <cafecafe51@hotmail.com>
2025-02-25 15:45:50 -0300

    feat: new button show all on the end of homepage featured products

--- alexa-site/src/app/components/homePage/FeaturedProducts.tsx
+++ alexa-site/src/app/components/homePage/FeaturedProducts.tsx
@@ -1,13 +1,15 @@
 import { FireBaseDocument, ProductBundleType } from '@/app/utils/types';
 import ProductCard from '../ProductList/ProductCard';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function FeaturedProducts({ featuredProducts }: { featuredProducts: (ProductBundleType & FireBaseDocument)[] }) {
     const hasProducts = Array.isArray(featuredProducts) && featuredProducts.length > 0;
 
     return (
-        <section className="py-8 sm:py-12 md:py-16 px-4 bg-[#F8C3D3] bg-opacity-20">
+        <section className="py-8 sm:py-12 md:py-16 px-4 bg-[#F8C3D3] bg-opacity-20 flex flex-col items-center">
             <div className="mx-auto">
-                <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6 sm:mb-8 md:mb-12">DESTAQUES</h2>
+                <h2 className="font-normal text-3xl text-center mb-6 sm:mb-8 md:mb-12">DESTAQUES</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 sm:gap-6 md:gap-8 min-h-[300px]">
                     { hasProducts ? (
                         featuredProducts.map((product) => (
@@ -28,6 +30,18 @@ export default function FeaturedProducts({ featuredProducts }: { featuredProduct
                     ) }
                 </div>
             </div>
+            <Link
+                href="/section"
+
+            >
+                <Button
+                    variant="outline"
+                    className="m-auto text-lg border-[#C48B9F] text-[#C48B9F] hover:bg-[#C48B9F] hover:text-white px-4 py-2 mt-8"
+                >
+                Ver Todas as Peças
+                </Button>
+            </Link>
+
         </section>
     );
 }
