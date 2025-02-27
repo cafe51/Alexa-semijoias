fa4536efb7ef092e48934b39e69cced79ab47d49
cafe51 <cafecafe51@hotmail.com>
2025-02-27 15:02:39 -0300

    fix style: esgotado tag z index

--- alexa-site/src/app/components/ProductList/OutOfStockLayer.tsx
+++ alexa-site/src/app/components/ProductList/OutOfStockLayer.tsx
@@ -2,7 +2,7 @@ export default function OutOfStockLayer({ outOfStock }: { outOfStock: boolean })
     return (
         outOfStock ?
             (
-                <div className="absolute top-0 right-0 w-52 h-52 overflow-hidden z-50">
+                <div className="absolute top-0 right-0 w-52 h-52 overflow-hidden z-40">
                     <div className="bg-[#C48B9F] bg-opacity-100 text-white font-bold py-1 text-center transform rotate-45 translate-y-8 translate-x-6 w-60">
                     ESGOTADO
                     </div>
