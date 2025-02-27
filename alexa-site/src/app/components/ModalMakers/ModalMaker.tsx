b2cfcb74319faa6a45dcfc5897c6c61f0a195fef
cafe51 <cafecafe51@hotmail.com>
2025-02-27 18:08:04 -0300

    fix style: modal width for lg screens

--- alexa-site/src/app/components/ModalMakers/ModalMaker.tsx
+++ alexa-site/src/app/components/ModalMakers/ModalMaker.tsx
@@ -17,7 +17,7 @@ export default function ModalMaker({ children, closeModelClick, title }: ModalMa
     };
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 w-full px-4" onClick={ handleBackdropClick }>
-            <div className="bg-white p-6 rounded-md shadow-md w-full md:w-2/3 lg:w-3/4 max-h-screen relative overflow-hidden">
+            <div className="bg-white p-6 rounded-md shadow-md w-full md:w-2/3 lg:w-1/3 max-h-screen relative overflow-hidden">
                 <ModalHeader closeModelClick={ closeModelClick } title={ title }/>
                 <div className="overflow-auto max-h-[calc(100vh-200px)] w-full">
                     { children }
