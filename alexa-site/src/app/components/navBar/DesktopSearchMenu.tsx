5c14dfb121b236396f501d8b8e450a283d97b650
cafe51 <cafecafe51@hotmail.com>
2025-03-10 12:46:08 -0300

    feat: show all link on search section modal

--- alexa-site/src/app/components/navBar/DesktopSearchMenu.tsx
+++ alexa-site/src/app/components/navBar/DesktopSearchMenu.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import SearchBar from '../header/SearchBar';
 import ProductCardsList from '../ProductList/ProductCardsList';
+import Link from 'next/link';
 
 export default function DesktopSearchMenu() {
     const [searchTerm, setSearchTerm] = useState('');
@@ -20,6 +21,11 @@ export default function DesktopSearchMenu() {
                     itemsPerPage={ 4 }
                     isMobileLayout
                 />
+                <Link
+                    className='hover:border-t-0 hover:border-x-0 hover:border-b-2 hover:border-[#F8C3D3] text-center text-3xl self-center mt-4'
+                    href={ `/search/${searchTerm}` }>
+                    Mostrar todos os resultados
+                </Link>
             </div> }
         </section>
     );
