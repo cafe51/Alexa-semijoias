cddf35e1b84d5aaa84dbeb4aa22b85931f9f0751
cafe51 <cafecafe51@hotmail.com>
2025-03-19 08:41:05 -0300

    fix: util function to accept undefine arg

--- alexa-site/src/app/utils/getImageUrlFromFirebaseProductDocument.tsx
+++ alexa-site/src/app/utils/getImageUrlFromFirebaseProductDocument.tsx
@@ -1,7 +1,7 @@
 import blankImage from '@/../public/blankImage.png';
 import { ProductBundleType } from './types';
 
-export function getImageUrlFromFirebaseProductDocument(product: ProductBundleType | null) {
+export function getImageUrlFromFirebaseProductDocument(product: ProductBundleType | null | undefined) {
     if(product && product.images && product.images[0]) {
         return product.images[0].localUrl;
     } else {
