1b88eb0688f607af3518c43e24c8a34532fd2aee
cafe51 <cafecafe51@hotmail.com>
2025-03-20 11:01:55 -0300

    style: translate embla class to tailwind on image carousel product page

--- alexa-site/src/app/components/ImageCarousel.tsx
+++ alexa-site/src/app/components/ImageCarousel.tsx
@@ -55,15 +55,26 @@ type PropType = {
 export const Thumb: React.FC<PropType> = (props) => {
     const { selected, productData, image, onClick } = props;
   
+    const emblaThumbsSlideClassName = 'min-w-0 flex-[0_0_22%] pl-[var(--thumbs-slide-spacing)] ' + 'min-[576px]:flex-[0_0_15%]';
+
     return (
         <div
-            className={ 'embla-thumbs__slide'.concat(
-                selected ? ' embla-thumbs__slide--selected' : '',
+            className={ emblaThumbsSlideClassName.concat(
+                selected ? ' text-[var(--text-body)]' : '',
             ) }
         >
-            <div
+            <button
                 onClick={ onClick }
-                className="embla-thumbs__slide__number"
+                type='button'
+                className="rounded-[1.8rem] bg-transparent touch-manipulation inline-flex items-center justify-center w-full h-[var(--thumbs-slide-height)] text-[1.8rem] font-semibold border-0 p-0 m-0 cursor-pointer select-none"
+                style={ {
+                    WebkitTapHighlightColor: 'rgba(var(--text-high-contrast-rgb-value), 0.5)',
+                    WebkitAppearance: 'none',
+                    appearance: 'none',
+                    textDecoration: 'none',
+                    boxShadow: 'inset 0 0 0 0.2rem var(--detail-medium-contrast)',
+                    color: 'var(--detail-high-contrast)',
+                } }
             >
                 <div className="relative w-full h-full bg-skeleton">
                     <Image
@@ -84,7 +95,7 @@ export const Thumb: React.FC<PropType> = (props) => {
                         quality={ 75 } // reduz um pouco a qualidade para diminuir o tamanho do arquivo
                     />
                 </div>
-            </div>
+            </button>
         </div>
     );
 };
@@ -121,20 +132,51 @@ export default function ImageCarousel({ productData, options }: ImageCarouselPro
     }, [emblaMainApi, onSelect]);
 
     return (
-        <section className="emblaProductPage">
-            <div className="embla__viewport" ref={ emblaMainRef }>
-                <div className="embla__container">
+        <section
+            className="max-w-[70rem] mx-auto"
+            style={ {
+                '--slide-height': '19rem',
+                '--slide-spacing': '1rem',
+                '--slide-size': '100%',
+                '--slide-spacing-sm': '1.6rem',
+                '--slide-size-sm': '100%', // calc(100% / 1) equivale a 100%
+                '--slide-spacing-lg': '2rem',
+                '--slide-size-lg': '100%',
+            } as React.CSSProperties }
+        >
+            <div className="overflow-hidden" ref={ emblaMainRef }>
+                <div
+                    className="flex ml-[calc(var(--slide-spacing)*-1)] min-[750px]:ml-[calc(var(--slide-spacing-sm)*-1)] min-[1200px]:ml-[calc(var(--slide-spacing-lg)*-1)]"
+                    style={ { touchAction: 'pan-y pinch-zoom', backfaceVisibility: 'hidden' } }
+                >
                     { productData.images.map((image, index) => (
-                        <div className="embla__slide" key={ index }>
+                        <div
+                            key={ index }
+                            className={
+                                'min-w-0 ' +
+                                'pl-[var(--slide-spacing)] ' +
+                                'flex-[0_0_var(--slide-size)] ' +
+                                'min-[750px]:pl-[var(--slide-spacing-sm)] ' +
+                                'min-[750px]:flex-[0_0_var(--slide-size-sm)] ' +
+                                'min-[1200px]:pl-[var(--slide-spacing-lg)] ' +
+                                'min-[1200px]:flex-[0_0_var(--slide-size-lg)]'
+                            }
+                        >
                             <ImageCarouselProduct productData={ productData } image={ image } />
                         </div>
                     )) }
                 </div>
             </div>
 
-            <div className="embla-thumbs">
-                <div className="embla-thumbs__viewport" ref={ emblaThumbsRef }>
-                    <div className="embla-thumbs__container">
+            <div
+                className="mt-[var(--thumbs-slide-spacing)]"
+                style={ {
+                    '--thumbs-slide-spacing': '0.8rem',
+                    '--thumbs-slide-height': '6rem',
+                } as React.CSSProperties }
+            >
+                <div className="overflow-hidden" ref={ emblaThumbsRef }>
+                    <div className="flex flex-row ml-[calc(var(--thumbs-slide-spacing)*-1)]">
                         { productData.images.map((image, index) => (
                             <Thumb
                                 key={ index }
