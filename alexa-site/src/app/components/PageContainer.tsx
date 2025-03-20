c91b70dcc8a0ed94c428c1c87c079654352ed396
cafe51 <cafecafe51@hotmail.com>
2025-03-20 15:40:46 -0300

    style: pb on section page

--- alexa-site/src/app/components/PageContainer.tsx
+++ alexa-site/src/app/components/PageContainer.tsx
@@ -8,7 +8,7 @@ interface PageContainerProps {
 export default function PageContainer({ children }: PageContainerProps) {
     return (
         <div 
-            className="min-h-screen bg-[#FAF9F6] text-[#333333] w-full" 
+            className="min-h-screen bg-[#FAF9F6] text-[#333333] w-full pb-8 md:pb-12 lg:pb-16 xl:pb-18" 
             style={ { fontFamily: 'Montserrat, sans-serif' } }
         >
             <div className="mx-auto w-full">
