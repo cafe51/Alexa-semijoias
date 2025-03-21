e0df32825d23eaa5241c32695908168eb6d1f022
cafe51 <cafecafe51@hotmail.com>
2025-03-21 16:13:18 -0300

    feat: change card image when hover

--- alexa-site/src/app/components/ProductList/ProductCard.tsx
+++ alexa-site/src/app/components/ProductList/ProductCard.tsx
@@ -1,7 +1,7 @@
 // src/app/components/ProductList/ProductCard.tsx
 'use client';
 
-import React, { memo, useMemo } from 'react';
+import React, { memo, useMemo, useState } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { FireBaseDocument, ProductBundleType } from '@/app/utils/types';
 import { formatPrice } from '@/app/utils/formatPrice';
@@ -29,48 +29,59 @@ function ProductCard({ product, homePage = false }: ProductCardProps) {
     // Memoriza o slug do produto
     const productSlug = useMemo(() => createSlugName(product.name), [product.name]);
 
+    // Estado para controlar o índice da imagem atual
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    // Determina a imagem a ser exibida
+    const imageUrl =
+    product.images && product.images.length > 0
+        ? product.images[currentImageIndex].localUrl
+        : blankImage.src;
+
     return (
-        <Card className="flex flex-col h-full overflow-hidden transition-shadow duration-300 hover:shadow-lg shadow-none  bg-transparent border-none rounded-none">
-            <CardContent className="p-0 flex flex-col h-full ">
+        <Card className="flex flex-col h-full overflow-hidden transition-shadow duration-300 hover:shadow-lg shadow-none bg-transparent border-none rounded-none">
+            <CardContent className="p-0 flex flex-col h-full">
                 <Link
                     href={ `/product/${productSlug}` }
                     className="relative aspect-[4/5]"
                     aria-label={ `Ver detalhes da semijoia ${toTitleCase(product.name)}` }
                     title={ `Ver detalhes da semijoia ${toTitleCase(product.name)}` }
+                    onMouseEnter={ () => {
+                        if (product.images && product.images.length > 1) {
+                            setCurrentImageIndex(1);
+                        }
+                    } }
+                    onMouseLeave={ () => setCurrentImageIndex(0) }
                 >
                     <div className="relative w-full h-full bg-skeleton">
                         <Image
                             data-testid="product-link"
-                            className={ `rounded-none object-cover scale-100 ${(product.estoqueTotal <= 0 && !homePage) ? 'opacity-50' : ''}` }
-                            src={
-                                product.images && product.images[0]
-                                    ? product.images[0].localUrl
-                                    : blankImage.src
-                            }
+                            className={ `rounded-none object-cover scale-100 ${
+                                product.estoqueTotal <= 0 && !homePage ? 'opacity-50' : ''
+                            }` }
+                            src={ imageUrl }
                             alt={ `Foto de ${product.name}` }
                             title={ `Foto de ${product.name}` }
-                            sizes={ '3000px' }
+                            sizes="3000px"
                             fill
-                            placeholder="blur" // melhora a percepção de carregamento
+                            placeholder="blur"
                             blurDataURL={ blankImage.src }
-                            priority={ homePage } // imagens prioritárias na homepage
-                            // loading={ homePage ? 'eager' : 'lazy' }
-                            quality={ 75 } // reduz um pouco a qualidade para diminuir o tamanho do arquivo
+                            priority={ homePage }
+                            quality={ 75 }
                         />
                     </div>
                     <span className="sr-only">{ toTitleCase(product.name) }</span>
-                    <ProductCardBadges product={ product }/>
-                    { !homePage && <OutOfStockLayer outOfStock={ product.estoqueTotal <= 0 }/> }
-                    
+                    <ProductCardBadges product={ product } />
+                    { !homePage && <OutOfStockLayer outOfStock={ product.estoqueTotal <= 0 } /> }
                 </Link>
 
                 <div className="p-2 pb-0 flex flex-col flex-grow text-center">
-                    <h3 className="text-base md:text-lg lg:text-xl font-semibold mb-2 text-[#333333] line-clamp-2"> 
+                    <h3 className="text-base md:text-lg lg:text-xl font-semibold mb-2 text-[#333333] line-clamp-2">
                         { toTitleCase(product.name) }
                     </h3>
 
-                    <div className="flex flex-col mb-3 flex-grow justify-end items-center"> 
-                        <div className="flex flex-wrap items-baseline gap-2 justify-center ">
+                    <div className="flex flex-col mb-3 flex-grow justify-end items-center">
+                        <div className="flex flex-wrap items-baseline gap-2 justify-center">
                             { product.value.promotionalPrice > 0 && (
                                 <span className="text-base lg:text-lg text-gray-500 line-through">
                                     { formatPrice(product.value.price) }
@@ -79,24 +90,21 @@ function ProductCard({ product, homePage = false }: ProductCardProps) {
                             <span className="text-xl lg:text-2xl font-bold text-[#D4AF37]">
                                 { formatPrice(displayPrice) }
                             </span>
-
                         </div>
 
                         <p className="text-base lg:text-lg text-gray-600 mt-1 text-center">
                             <span className="font-semibold text-base lg:text-xl text-[#C48B9F]">
-                                6x
+                6x
                             </span>{ ' ' }
-                            de{ ' ' }
+              de{ ' ' }
                             <span className="font-semibold text-lg lg:text-2xl text-[#C48B9F]">
                                 { formatPrice(installmentValue) }
                             </span>{ ' ' }
-                            sem juros
+              sem juros
                         </p>
                     </div>
-
                 </div>
                 { !homePage && <BuyButtonOnCard product={ product } /> }
-
             </CardContent>
         </Card>
     );
