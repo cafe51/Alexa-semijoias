d75be217e82ad668c6f7f9fd9df20a06c65cddb5
cafe51 <cafecafe51@hotmail.com>
2025-04-01 12:44:57 -0300

    style: inner border on banner

--- alexa-site/src/app/components/ProductList/SectionBanner.tsx
+++ alexa-site/src/app/components/ProductList/SectionBanner.tsx
@@ -13,7 +13,6 @@ import ShareSection from '../ProductPage/ShareSection';
 import { createSlugName } from '@/app/utils/createSlugName';
 import { SITE_URL } from '@/app/utils/constants';
 
-
 interface SectionBannerProps {
     bannerImage?: string | null;
     bannerDescription?: string | null;
@@ -26,14 +25,11 @@ interface SectionBannerProps {
 export default function SectionBanner({
     bannerImage,
     bannerDescription,
-
     lastAddProduct,
     sectionName,
     subsection,
     collectionName,
 }: SectionBannerProps) {
-    // const title = subsection ? subsection : sectionName ? sectionName : 'Alexa Semijoias';
-
     const description = bannerDescription
         ? bannerDescription
         : lastAddProduct ? shortenText(lastAddProduct.description, 400) : '';
@@ -62,19 +58,22 @@ export default function SectionBanner({
     return (
         <section className="w-full grid md:grid-cols-[50%_auto] overflow-hidden pt-2 ">
             {       
-                <div className='hidden md:flex flex-col justify-between relative aspect-[10/8] md:aspect-auto  bg-[#C48B9F] w-full '>
-                    <div className="w-full flex justify-between items-center pr-2 md:pr-4 lg:pr-6 xl:pr-8 pt-4 text-white">  
-                        <Breadcrumbs items={ breadCrumbItems } textColorAllWhite/>
-                        <div className='text-white'>
-                            <ShareSection
-                                url={ linkToShareSection }
-                                callToAction='Compartilhe'
-                            />
+                // Renderização para telas maiores com borda interna na área de cor C48B9F
+                <div className="hidden md:flex flex-col justify-between relative aspect-[10/8] p-6 md:aspect-auto bg-[#C48B9F] w-full ">
+                    <div className="relative flex flex-col justify-between h-full w-full box-border border-4 border-[#F8C3D3] border-double">
+                        <div className="w-full flex justify-between items-center pr-2 md:pr-4 lg:pr-6 xl:pr-8 pt-4 text-white">  
+                            <Breadcrumbs items={ breadCrumbItems } textColorAllWhite/>
+                            <div className="text-white">
+                                <ShareSection
+                                    url={ linkToShareSection }
+                                    callToAction="Compartilhe"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className='flex flex-col justify-start items-start text-start text-lg px-4 md:p-8 lg:p-12 xl:p-16 gap-4'>
-                        <p className='text-white md:text-5xl lg:text-7xl'>{ toTitleCase(title) }</p> 
-                        <p className='text-white text-sm md:text-base lg:text-lg xl:text-xl'>{ description }</p>
+                        <div className="flex flex-col justify-start items-start text-start text-lg px-4 md:p-8 lg:p-12 xl:p-16 gap-4">
+                            <p className="text-white md:text-5xl lg:text-7xl">{ toTitleCase(title) }</p> 
+                            <p className="text-white text-sm md:text-base lg:text-lg xl:text-xl">{ description }</p>
+                        </div>
                     </div>
                 </div>
             }
@@ -87,37 +86,34 @@ export default function SectionBanner({
                     title="Banner Principal Alexa Semijoias"
                     alt="Banner Principal Alexa Semijoias"
                     priority
-                    // Usamos "100vw" para informar que a imagem ocupa toda a largura da viewport
                     sizes="1000px"
-                    quality={ 80 } // Se possível, teste reduzir um pouco esse valor (ex: 75) sem comprometer a qualidade visual
+                    quality={ 80 }
                     fill
                     placeholder="blur"
                     blurDataURL={ heroBannerLarge.blurDataURL }
                 />
                 
-                { /* Container do logo centralizado */ }
+                { /* Container do logo centralizado para mobile */ }
                 <div className="absolute md:hidden inset-0 flex flex-col items-between justify-between h-full "> 
                     <div className="w-full flex justify-between items-center pr-4 md:pr-8 lg:pr-12 xl:pr-16 pt-4 bg-black/30">
                         <Breadcrumbs items={ breadCrumbItems } textColorAllWhite/>
-
                     </div>
                         
-                    <div className='flex flex-col justify-center h-full bg-black/20'>
-                        <div className='flex flex-col justify-center md:justify-start items-center md:items-start text-center md:text-start text-lg p-4 md:p-8 lg:p-12 xl:p-16 gap-4 bg-black/10'>
-                            <p className='text-white text-4xl lg:text-7xl'>{ toTitleCase(title) }</p> 
-                            <p className='text-white text-base md:text-base'>{ description }</p>
+                    <div className="flex flex-col justify-center h-full bg-black/20">
+                        <div className="flex flex-col justify-center md:justify-start items-center md:items-start text-center md:text-start text-lg p-4 md:p-8 lg:p-12 xl:p-16 gap-4 bg-black/10">
+                            <p className="text-white text-4xl lg:text-7xl">{ toTitleCase(title) }</p> 
+                            <p className="text-white text-base md:text-base">{ description }</p>
                         </div>
                     </div>
 
-                    <div className='flex flex-col justify-end items-end p-4 w-full bg-black/30'>
-                        <div className='text-white'>
+                    <div className="flex flex-col justify-end items-end p-4 w-full bg-black/30">
+                        <div className="text-white">
                             <ShareSection
                                 url={ linkToShareSection }
-                                callToAction='Compartilhe'
+                                callToAction="Compartilhe"
                             />
                         </div>
                     </div>
-
                 </div>
             </div>
         </section>
--- alexa-site/src/app/components/homePage/HeroSection.tsx
+++ alexa-site/src/app/components/homePage/HeroSection.tsx
@@ -2,12 +2,10 @@
 // Removemos o 'use client' porque esse componente é estático e não interage com o estado
 import Image from 'next/image';
 import heroBannerLarge from '@/../public/heroBannerLarge.webp';
-// import bigHeroLogo from '@/../public/bigHeroLogo2.png';
 import { FireBaseDocument, ProductBundleType } from '@/app/utils/types';
 import toTitleCase from '@/app/utils/toTitleCase';
 import Link from 'next/link';
 import { getImageUrlFromFirebaseProductDocument } from '@/app/utils/getImageUrlFromFirebaseProductDocument';
-
 
 interface HeroSectionProps {
     lastAddProduct: ProductBundleType & FireBaseDocument | undefined;
@@ -23,11 +21,13 @@ export default function HeroSection({ lastAddProduct }: HeroSectionProps) {
 
     const titleOfBanner = subsectionName ? subsectionName : sectionName ? sectionName : 'Alexa Semijoias';
 
-    const linkToSection = (subsectionName && sectionName) ? `/section/${sectionName}/${subsectionName}` : sectionName ? `/section/${sectionName}` : '/section';
+    const linkToSection = (subsectionName && sectionName)
+        ? `/section/${sectionName}/${subsectionName}`
+        : sectionName ? `/section/${sectionName}` : '/section';
 
     return (
         <section className="w-full grid md:grid-cols-2 overflow-hidden">
-            { /* Container principal com altura fixa para desktop */ }
+            { /* Container da imagem */ }
             <div className="relative aspect-[10/9] md:aspect-[3/5] lg:aspect-[4/5] xl:aspect-[5/4] bg-skeleton">
                 <Image
                     className="object-cover"
@@ -35,57 +35,33 @@ export default function HeroSection({ lastAddProduct }: HeroSectionProps) {
                     title="Banner Principal Alexa Semijoias"
                     alt="Banner Principal Alexa Semijoias"
                     priority
-                    // Usamos "100vw" para informar que a imagem ocupa toda a largura da viewport
                     sizes="1000px"
-                    quality={ 80 } // Se possível, teste reduzir um pouco esse valor (ex: 75) sem comprometer a qualidade visual
+                    quality={ 80 }
                     fill
                     placeholder="blur"
                     blurDataURL={ heroBannerLarge.blurDataURL }
                 />
-                
-                { /* Container do logo centralizado */ }
-                { /* <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="relative w-[90%] md:w-[60%] lg:w-[50%] aspect-[3/1]">
-                        <Image
-                            className="object-contain"
-                            title="Logo Alexa Semijoias"
-                            src={ bigHeroLogo }
-                            alt="Logo Alexa Semijoias"
-                            priority
-                            sizes="(max-width: 640px) 90vw, (max-width: 1300px) 60vw, 50vw"
-                            quality={ 100 }
-                            fill
-                            placeholder="blur"
-                            blurDataURL={ bigHeroLogo.blurDataURL }
-                        />
-                    </div>
-                </div> */ }
             </div>
-            { /* 3F4A2D */ }
-            { /* C48B9F */ }
-            {       
-                <div className="relative aspect-square sm:aspect-[10/7] md:aspect-[3/5] lg:aspect-[4/5] xl:aspect-[5/4] bg-[#C48B9F] flex flex-col items-center justify-center gap-4 pt-4 md:pt-8">
-                    <h3 className='text-white text-lg uppercase tracking-widest'>Semijoias de Verdade</h3>
-                    <div className='justify-around flex flex-col items-center gap-4'>
-                        <div className='text-center text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
-                            <p className='text-white'>{ toTitleCase(titleOfBanner) }</p>
-                            <p className='text-white'>Para Transformar</p>
-                            <p className='text-white'>Seu Estilo</p>
+            { /* Container com fundo e efeito de borda interna */ }
+            <div className="relative aspect-square sm:aspect-[10/7] md:aspect-[3/5] lg:aspect-[4/5] xl:aspect-[5/4] bg-[#C48B9F] p-2 md:p-6">
+                <div className="relative flex flex-col items-center justify-center gap-4 h-full w-full box-border border-4 border-[#F8C3D3] border-double ">
+                    <h3 className="text-white text-lg uppercase tracking-widest">
+                        Semijoias de Verdade
+                    </h3>
+                    <div className="flex flex-col items-center justify-around gap-4">
+                        <div className="text-center text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+                            <p className="text-white">{ toTitleCase(titleOfBanner) }</p>
+                            <p className="text-white">Para Transformar</p>
+                            <p className="text-white">Seu Estilo</p>
                         </div>
-                        <Link
-                            href={ linkToSection }
-
-                        >
-                            <button
-                                className='bg-white font-bold text-[#C48B9F] p-4 px-14 rounded-full md:text-xl  hover:bg-[#D4AF37] hover:text-white transition-colors duration-300'
-                            > 
-                            Descubra
+                        <Link href={ linkToSection }>
+                            <button className="bg-white font-bold text-[#C48B9F] p-4 px-14 rounded-full md:text-xl hover:bg-[#D4AF37] hover:text-white transition-colors duration-300">
+                                Descubra
                             </button>
                         </Link>
-
                     </div>
                 </div>
-            }
+            </div>
         </section>
     );
 }
