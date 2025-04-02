d52859975b34906e495556991aa1a5d4bea18e79
cafe51 <cafecafe51@hotmail.com>
2025-04-02 11:35:26 -0300

    style: aspect ration of hero banner for smaller screens

--- alexa-site/src/app/components/homePage/HeroSection.tsx
+++ alexa-site/src/app/components/homePage/HeroSection.tsx
@@ -27,7 +27,15 @@ export default function HeroSection({ lastAddProduct }: HeroSectionProps) {
     return (
         <section className="w-full grid md:grid-cols-2 overflow-hidden">
             { /* Container da imagem */ }
-            <div className="relative aspect-[10/9] md:aspect-[3/5] lg:aspect-[4/5] xl:aspect-[5/4] bg-skeleton">
+            <div className="relative
+            aspect-[16/15]
+            min-[440px]:aspect-[10/9]
+            
+            sm:aspect-[10/7]
+            md:aspect-[3/5]
+            lg:aspect-[4/5]
+            xl:aspect-[5/4]
+            bg-skeleton">
                 <Image
                     className="object-cover"
                     src={ getImageUrlFromFirebaseProductDocument(lastAddProduct) }
@@ -42,19 +50,32 @@ export default function HeroSection({ lastAddProduct }: HeroSectionProps) {
                 />
             </div>
             { /* Container com fundo e efeito de borda interna */ }
-            <div className="relative aspect-square sm:aspect-[10/7] md:aspect-[3/5] lg:aspect-[4/5] xl:aspect-[5/4] bg-[#C48B9F] p-2 md:p-6">
-                <div className="relative flex flex-col items-center justify-center gap-4 h-full w-full box-border border-4 border-[#F8C3D3] border-double ">
-                    <h3 className="text-white text-lg uppercase tracking-widest">
+            <div className="
+            relative
+            aspect-[16/11] bg-[#C48B9F]
+            min-[440px]:aspect-[10/9] 
+
+            sm:aspect-[10/7] 
+            md:aspect-[3/5]
+            lg:aspect-[4/5] 
+            xl:aspect-[5/4]
+            p-2 md:p-6">
+                <div className="relative flex flex-col items-center justify-center gap-1 min-[440px]:gap-4 h-full w-full box-border border-4 border-[#F8C3D3] border-double ">
+                    <h3 className="text-white text-xs min-[440px]:text-base md:text-lg lg:text-lg xl:text-xl uppercase tracking-widest">
                         Semijoias de Verdade
                     </h3>
                     <div className="flex flex-col items-center justify-around gap-4">
                         <div className="text-center ">
-                            <p className="text-white text-3xl md:text-4xl lg:text-5xl xl:text-7xl mb-2 xl:mb-4">{ toTitleCase(titleOfBanner) }</p> 
-                            <p className="text-white text-2xl md:text-3xl lg:text-4xl xl:text-6xl">Para Transformar</p>
-                            <p className="text-white text-2xl md:text-3xl lg:text-4xl xl:text-6xl">Seu Estilo</p>
+                            <p className="text-white text-2xl min-[440px]:text-4xl md:text-4xl lg:text-5xl xl:text-7xl mb-2 xl:mb-4">{ toTitleCase(titleOfBanner) }</p> 
+                            <p className="text-white text-xl min-[440px]:text-3xl md:text-3xl lg:text-4xl xl:text-6xl">Para Transformar</p>
+                            <p className="text-white text-xl min-[440px]:text-3xl md:text-3xl lg:text-4xl xl:text-6xl">Seu Estilo</p>
                         </div>
                         <Link href={ linkToSection }>
-                            <button className="bg-white font-bold text-[#C48B9F] p-4 px-14 rounded-full md:text-xl hover:bg-[#D4AF37] hover:text-white transition-colors duration-300">
+                            <button className="
+                            bg-white font-bold text-[#C48B9F]
+                            p-4 px-14 rounded-full
+                            min-[440px]:text-xl 
+                            hover:bg-[#D4AF37] hover:text-white transition-colors duration-300">
                                 Descubra
                             </button>
                         </Link>
