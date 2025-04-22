53cd077fb9588ce30d50cba31e7e6d3bd78d543e
cafe51 <cafecafe51@hotmail.com>
2025-04-22 12:17:45 -0300

    feat: category badge are now links to search page

--- alexa-site/src/app/components/ProductPage/ProductCategories.tsx
+++ alexa-site/src/app/components/ProductPage/ProductCategories.tsx
@@ -1,14 +1,17 @@
 import { FireBaseDocument, ProductBundleType } from '@/app/utils/types';
 import { Badge } from '@/components/ui/badge';
+import Link from 'next/link';
 
 
 export default function ProductCategories({ product }: {product: ProductBundleType & FireBaseDocument}) {
     return (
         <div className="flex flex-wrap items-center gap-2 mb-4">
             { product.categories.map((category, index) => (
-                <Badge key={ index } variant="secondary" className="bg-[#F8C3D3] text-[#333333]">
-                    { category }
-                </Badge>
+                <Link key={ index } href={ `/search/${ encodeURIComponent(category) }` }>
+                    <Badge variant="secondary" className="bg-[#F8C3D3] text-[#333333]">
+                        { category }
+                    </Badge>
+                </Link>
             )) }
             { product.lancamento && <Badge variant="destructive" className="bg-[#C48B9F] text-white">Lançamento</Badge> }
         </div>
